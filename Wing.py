5a5b791b6a638e37a220f00a99094b3af27c4afa
Peter Heatwole <pheatwole@gmail.com>
2018-02-16 12:09:08 -0800

    Add Wing moments of inertial calcuations

--- Wing.py
+++ Wing.py
@@ -1,11 +1,12 @@
 import numpy as np
-from numpy import sqrt, sin, cos, tan, arctan, deg2rad
+from numpy import sqrt, sin, cos, tan, arcsin, arctan, deg2rad
 
 
 class Wing:
-    def __init__(self, geometry, airfoil):
+    def __init__(self, geometry, airfoil, wing_density=0.2):
         self.geometry = geometry
         self.airfoil = airfoil
+        self.wing_density = wing_density  # FIXME: no idea in general
 
     def fE(self, y, xa=None, N=150):
         """Airfoil upper camber line on the 3D wing
@@ -70,6 +71,33 @@ class Wing:
                 )
         return np.c_[x, _y, z]
 
+    def J(self, rho=1.3, N=2000):
+        """Compute the 3x3 moment of inertia matrix.
+
+        Parameters
+        ----------
+        rho : float
+            Volumetric air density of the atmosphere
+        N : integer
+            The number of points for integration across the span
+
+        Returns
+        -------
+        J : 3x3 matrix of float
+                [[Jxx Jxy Jxz]
+            J =  [Jxy Jyy Jyz]
+                 [Jxz Jyz Jzz]]
+        """
+        S = self.geometry.surface_distributions(N=N)
+        wing_air_density = rho*self.density_factor
+        surface_density = self.wing_density + wing_air_density
+        return surface_density * S
+
+    @property
+    def density_factor(self):
+        # FIXME: I don't understand this. Ref: PFD 48 (46)
+        return self.geometry.mac * self.airfoil.t/3
+
 
 class WingGeometry:
     def __init__(self, c0, h0, dcg):
@@ -85,6 +113,21 @@ class WingGeometry:
         self.c0 = c0
         self.h0 = h0
         self.dcg = dcg
+
+    @property
+    def S(self):
+        """Projected surface area"""
+        raise NotImplementedError("WingGeometry is a base class")
+
+    @property
+    def AR(self):
+        """Aspect ratio"""
+        raise NotImplementedError("WingGeometry is a base class")
+
+    @property
+    def mac(self):
+        """Mean aerodynamic chord"""
+        raise NotImplementedError("WingGeometry is a base class")
 
     def fx(self, y):
         """The quarter chord projected onto the XY plane"""
@@ -102,6 +145,45 @@ class WingGeometry:
         """Spanwise airfoil chord angle relative to the central airfoil"""
         raise NotImplementedError("WingGeometry is a base class")
 
+    def surface_distributions(self, N=2000):
+        """The surface area distributions for computing inertial moments.
+
+        The moments of inertia for the wing are the mass distribution of the
+        air and wing material. That distribution is typically decomposed into
+        the product of volumetric density and volume, but a simplification is
+        to calculate the density per unit area.
+
+        FIXME: this description is mediocre.
+
+        Ref: "Paraglider Flight Dynamics", page 48 (56)
+
+        Returns
+        ------
+        S : 3x3 matrix of float
+            The surface distributions, such that `J = (p_w + p_air)*s`
+        """
+        dy = 1/N
+        y = np.linspace(-self.b/2, self.b/2, N-1) + dy/2
+        fx = self.fx(y)
+        fz = self.fz(y)
+        fc = self.fc(y)
+
+        # FIXME: needs verification
+        # FIXME: this is a crude rectangle rule integration
+        Sx = ((y**2 + fz**2)*fc).sum()*dy
+        Sy = ((3*fx**2 - fx*fc + (7/32)*fc**2 + 6*fz**2)*fc).sum()*dy
+        Sz = ((3*fx**2 - fx*fc + (7/32)*fc**2 + 6*y**2)*fc).sum()*dy
+        Sxy = 0
+        Sxz = ((2*fx - fc/2)*fz*fc).sum()*dy
+        Syz = 0
+
+        S = np.array([
+            [Sx, Sxy, Sxz],
+            [Sxy, Sy, Syz],
+            [Sxz, Syz, Sz]])
+
+        return S
+
 
 class EllipticalWing(WingGeometry):
     """Ref: Paraglider Flying Dynamics, page 43 (51)"""
@@ -118,6 +200,25 @@ class EllipticalWing(WingGeometry):
         self.b = b
         self.torsion = deg2rad(torsion)
         self.taper = taper
+
+    @property
+    def S(self):
+        # ref: PDF 46 (54)
+        t = self.taper
+        taper_factor = t + arcsin(sqrt(1 - t**2))/sqrt(1 - t**2)
+        return self.c0 * self.b/2 * taper_factor
+
+    @property
+    def AR(self):
+        t = self.taper
+        taper_factor = t + arcsin(sqrt(1 - t**2))/sqrt(1 - t**2)
+        return 2 * self.b / (self.c0*taper_factor)
+
+    @property
+    def mac(self):
+        t = self.taper
+        taper_factor = t + arcsin(sqrt(1 - t**2))/sqrt(1 - t**2)
+        return (2/3) * self.c0 * (2 + t**2) / taper_factor
 
     def fx(self, y):
         tMed = tan(self.sweepMed)
