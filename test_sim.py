8f30cc8f78f54472fb31d276ecb22939d013ba8c
Peter Heatwole <pheatwole@gmail.com>
2019-12-19 16:44:51 -0800

    Simplify `test_sim.py` and reuse Hook 3 definition

--- test_sim.py
+++ test_sim.py
@@ -1,141 +1,14 @@
 import time
 
-import matplotlib.pyplot as plt
-import numpy as np
-from numpy import sin, cos
-from numpy.linalg import norm
 from IPython import embed
-import pandas as pd
-from scipy.interpolate import UnivariateSpline
+
+import numpy as np
+
 import scipy.integrate
 
-import Airfoil
-import Parafoil
-import BrakeGeometry
-import Harness
-from ParagliderWing import ParagliderWing
-from Paraglider import Paraglider
+import hook3
 import quaternion
-
 from util import cross3
-
-
-def build_elliptical_parafoil(b_flat, taper, dMed, sMed, airfoil,
-                              SMC=None, MAC=None,
-                              dMax=None, sMax=None,
-                              torsion_max=0, torsion_exponent=6):
-
-    if SMC is None and MAC is None:
-        raise ValueError("One of the SMC or MAC are required")
-
-    if dMed > 0 or (dMax is not None and dMax > 0):
-        raise ValueError("dihedral must be negative")
-
-    if sMed < 0 or (sMax is not None and sMax < 0):
-        raise ValueError("sweep must be positive")  # FIXME: why?
-
-    if dMax is None:
-        dMax = 2*dMed - 1  # ref page 48 (56)
-        print(f"Using minimum max dihedral ({dMax})")
-
-    if sMax is None:
-        sMax = (2*sMed) + 1  # ref page 48 (56)
-        print(f"Using minimum max sweep ({sMax})")
-
-    if SMC is not None:
-        c0 = Parafoil.EllipticalPlanform.SMC_to_c0(SMC, taper)
-    else:
-        c0 = Parafoil.EllipticalPlanform.MAC_to_c0(MAC, taper)
-
-    planform = Parafoil.EllipticalPlanform(
-        b_flat, c0, taper, sMed, sMax, torsion_exponent, torsion_max)
-    lobe = Parafoil.EllipticalLobe(dMed, dMax)
-
-    return Parafoil.ParafoilGeometry(planform, lobe, airfoil)
-
-
-class FlaplessAirfoilCoefficients(Airfoil.AirfoilCoefficients):
-    """
-    Uses the airfoil coefficients from a CSV file.
-    The CSV must contain the following columns: [alpha, delta, CL, CD, Cm]
-
-    This is similar to `Airfoil.GridCoefficients`, but it assumes that delta
-    is always zero. This is convenient, since no assuptions need to be made
-    for the non-existent flaps on the wind tunnel model.
-    """
-
-    def __init__(self, filename, convert_degrees=True):
-        data = pd.read_csv(filename)
-        self.data = data
-
-        if convert_degrees:
-            data['alpha'] = np.deg2rad(data.alpha)
-
-        self._Cl = UnivariateSpline(data[['alpha']], data.CL, s=0.001)
-        self._Cd = UnivariateSpline(data[['alpha']], data.CD, s=0.0001)
-        self._Cm = UnivariateSpline(data[['alpha']], data.Cm, s=0.0001)
-        self._Cl_alpha = self._Cl.derivative()
-
-    def _clean(self, alpha, val):
-        # The UnivariateSpline doesn't fill `nan` outside the boundaries
-        min_alpha, max_alpha = np.deg2rad(-9.9), np.deg2rad(24.9)
-        mask = (alpha < min_alpha) | (alpha > max_alpha)
-        val[mask] = np.nan
-        return val
-
-    def Cl(self, alpha, delta):
-        return self._clean(alpha, self._Cl(alpha))
-
-    def Cd(self, alpha, delta):
-        return self._clean(alpha, self._Cd(alpha))
-
-    def Cm(self, alpha, delta):
-        return self._clean(alpha, self._Cm(alpha))
-
-    def Cl_alpha(self, alpha, delta):
-        return self._clean(alpha, self._Cl_alpha(alpha))
-
-
-def build_glider():
-    print("\nAirfoil: NACA 24018, curving flap")
-    airfoil_geo = Airfoil.NACA(24018, convention='british')
-    airfoil_coefs = Airfoil.GridCoefficients('polars/exp_curving_24018.csv')
-    delta_max = np.deg2rad(13.25)  # raw delta_max = 13.38
-
-    # print("\nAirfoil: NACA 23015, flapless (no support for delta)")
-    # airfoil_geo = Airfoil.NACA(24018, convention='british')
-    # airfoil_coefs = FlaplessAirfoilCoefficients(
-    #     'polars/NACA 23015_T1_Re0.920_M0.03_N7.0_XtrTop 5%_XtrBot 5%.csv')
-    # delta_max = np.deg2rad(0)  # Flapless coefficients don't support delta
-
-    airfoil = Airfoil.Airfoil(airfoil_coefs, airfoil_geo)
-
-    # Hook3 specs:
-    S_flat, b_flat, AR_flat = 23, 11.15, 5.40  # noqa: F841
-    SMC_flat = b_flat/AR_flat
-    S, b, AR = 19.55, 8.84, 4.00  # noqa: F841
-
-    parafoil = build_elliptical_parafoil(   # Hook 3 (ish)
-        b_flat=b_flat, SMC=SMC_flat, taper=0.35, dMed=-32, dMax=-75,
-        sMed=13.5, sMax=40, torsion_max=0, airfoil=airfoil)
-
-    p_start, p_peak = 0, 0.75
-    brakes = BrakeGeometry.Cubic(p_start, p_peak, delta_max)
-
-    wing = ParagliderWing(parafoil, Parafoil.Phillips, brakes,
-                          d_riser=0.50, z_riser=6.8,
-                          pA=0.08, pC=0.80,
-                          kappa_s=0.15)
-
-    # The 6 DoF glider model holds the harness orientation fixed, so if z_riser
-    # is non-zero it will introduce an unnatural torque.
-    harness = Harness.Spherical(mass=75, z_riser=0.0, S=0.55, CD=0.8)
-
-    glider = Paraglider(wing, harness)
-
-    return glider
-
-# ---------------------------------------------------------------------------
 
 
 class GliderSim:
@@ -171,9 +44,9 @@ class GliderSim:
             The array of N state components
         params : dictionary
             Any extra non-state parameters for computing the dynamics. Be aware
-            that 'Gamma' an in-out parameter: solutions for the current Gamma
-            distribution are passed forward (output) to be used as the proposal
-            to the next time step.
+            that 'solution' is an in-out parameter: solutions for the current
+            Gamma distribution are passed forward (output) to be used as the
+            proposal to the next time step.
 
         Returns
         -------
@@ -200,15 +73,15 @@ class GliderSim:
         # FIXME: Paraglider should return accelerations directly, not forces.
         #        The Glider might want to utilize appparent inertia, etc.
         v_frd = quaternion.apply_quaternion_rotation(x['q'], x['v'])
-        F, M, Gamma = self.glider.forces_and_moments(
-            v_frd, x['omega'], g, rho=rho_air,
+        F, M, solution = self.glider.forces_and_moments(
+            v_frd, x['omega'], g, rho_air=rho_air,
             delta_s=delta_s, delta_Bl=delta_Bl, delta_Br=delta_Br,
-            Gamma=params['Gamma'])
+            reference_solution=params['solution'])
 
         # FIXME: what if Phillips fails? How do I abort gracefully?
 
         # Translational acceleration of the cm
-        a_frd = F/self.glider.harness.mass  # FIXME: crude, incomplete
+        a_frd = F / self.glider.harness.mass  # FIXME: crude, incomplete
         a_ned = quaternion.apply_quaternion_rotation(q_inv, a_frd)
 
         # Angular acceleration of the body relative to the ned frame
@@ -231,8 +104,8 @@ class GliderSim:
         x_dot['v'] = a_ned
         x_dot['omega'] = alpha
 
-        # Save the Gamma distribution for use with the next time update
-        params['Gamma'] = Gamma  # FIXME: needs a design review
+        # Use the solution as the reference_solution at the next time step
+        params['solution'] = solution  # FIXME: needs a design review
 
         return x_dot.view(float)  # The integrator expects a flat array
 
@@ -275,7 +148,7 @@ def simulate(model, state0, T=10, T0=0, dt=0.5, first_step=0.25, max_step=0.5):
     solver = scipy.integrate.ode(model.dynamics)
     solver.set_integrator('dopri5', rtol=1e-3, first_step=0.25, max_step=0.5)
     solver.set_initial_value(state0.view(float))
-    solver.set_f_params({'Gamma': None})  # Is changed by `model.dynamics`
+    solver.set_f_params({'solution': None})  # Is modified by `model.dynamics`
 
     t_start = time.time()
     msg = ''
@@ -313,10 +186,11 @@ def simulate(model, state0, T=10, T0=0, dt=0.5, first_step=0.25, max_step=0.5):
 
     # For debugging
     eulers = np.rad2deg(quaternion.quaternion_to_euler(path['q']))
-    cps = model.glider.control_points(0)  # Control points in frd
-    cp0 = cps[95]  # The central control point in frd
+    cps = model.glider.control_points(0)  # Control points in FRD (wing+harness)
+    cp0 = cps[(cps.shape[0] - 1) // 2]  # The central control point in frd
     p_cp0 = path['p'] + quaternion.apply_quaternion_rotation(path['q'], cp0)
     v_cp0 = path['v'] + cross3(path['omega'], cp0)
+    Phi = quaternion.quaternion_to_euler(path['q'])  # FIXME: correct?
 
     embed()
 
@@ -324,17 +198,25 @@ def simulate(model, state0, T=10, T0=0, dt=0.5, first_step=0.25, max_step=0.5):
 
 
 def main():
-    glider = build_glider()
-    model = GliderSim(glider, 1.2)
+    glider = hook3.build_hook3()
+    rho_air = 1.2
+    model = GliderSim(glider, rho_air=rho_air)
 
-    alpha_eq, Theta_eq, V_eq = glider.equilibrium_glide(0, 0, 1.2)
+    # Start the wing in an equilibrium state (assuming zero wind)
+    # alpha, Theta, V = glider.equilibrium_glide(0, 0, rho_air=rho_air)
+
+    # Start the wing in a random state (non-equilibrium)
+    alpha = np.deg2rad(8.5)
+    beta = np.deg2rad(0)
+    Theta = np.deg2rad(3)
+    V = 10.5
 
     # Build some data
-    alpha, beta = np.deg2rad(8.5), np.deg2rad(0)
-    UVW = 10.5 * np.asarray(
-        [cos(alpha)*cos(beta), sin(beta), sin(alpha)*cos(beta)])
+    UVW = V * np.asarray(
+        [np.cos(alpha) * np.cos(beta), np.sin(beta), np.sin(alpha) * np.cos(beta)])
     PQR = [np.deg2rad(0), np.deg2rad(0), np.deg2rad(0)]  # omega [rad/sec]
     Phi = [np.deg2rad(0), np.deg2rad(0), np.deg2rad(0)]  # attitude [rad]
+    # FIXME: make attitude a function of Theta
 
     # q = np.array([1, 0, 0, 0])  # The identity quaternion (zero attitude)
     q = quaternion.euler_to_quaternion(Phi)  # Encodes C_frd/ned
@@ -357,7 +239,7 @@ def main():
 
     # For verification purposes
     # delta_PE = 9.8 * 75 * -path['p'].T[2]  # PE = mgh
-    # KE_trans = 1/2 * 75 * norm(path['v'], axis=1)**2  # translational KE
+    # KE_trans = 1/2 * 75 * np.linalg.norm(path['v'], axis=1)**2  # translational KE
     # KE_rot = 1/2 * np.einsum('ij,kj->k', J, path['omega']**2)
     # delta_E = delta_PE + (KE_trans - KE_trans[0]) + KE_rot
 
