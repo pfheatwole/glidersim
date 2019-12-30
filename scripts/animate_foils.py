47cf702b13219c23204dc698173d4f3b30d340e7
Peter Heatwole <pheatwole@gmail.com>
2019-12-30 13:14:46 -0800

    WIP: add animation sequences

--- scripts/animate_foils.py
+++ scripts/animate_foils.py
@@ -1,259 +1,350 @@
+from collections import ChainMap
+
 import matplotlib.pyplot as plt
+from matplotlib import animation
 from mpl_toolkits.mplot3d import Axes3D  # noqa: F401; for `projection='3d'`
+
 import numpy as np
 from numpy import sin, cos, sqrt, arcsin, arctan  # noqa: F401
 
-from matplotlib import animation
-import mpl_toolkits.mplot3d.axes3d as p3
-
 from IPython import embed  # noqa: F401
 
-from Airfoil import Airfoil, LinearCoefficients, NACA
-from Wing import Wing, EllipticalWing
-from plots import plot_airfoil_geo
+import pfh.glidersim as gsim
+from pfh.glidersim.airfoil import Airfoil, NACA
+from pfh.glidersim.foil import (
+    elliptical_lobe,
+    elliptical_chord,
+    FlatYZ,
+    PolynomialTorsion,
+    SimpleIntakes,
+)
 
 
-def set_axes_equal(ax):
-    '''Make axes of 3D plot have equal scale so that spheres appear as spheres,
-    cubes as cubes, etc..  This is one possible solution to Matplotlib's
-    ax.set_aspect('equal') and ax.axis('equal') not working for 3D.
+def sweep(vstart, vstop, T, reverse=True, fps=60):
+    """
+    Generate a sequence of scalars.
 
-    Input
-      ax: a matplotlib axis, e.g., as output from plt.gca().
-    '''
+    name : string
+        The parameter to sweep across
+    vstart, vstop : float
+        The starting and stopping values of the sweep
+    T : float [seconds]
+        Total duration of the sweep. If `reverse = True`, then the forward and
+        reverse sequences take `T / 2` seconds.
+    fps : float [Hz]
+        Frames per second
+    """
+    if reverse:
+        T /= 2
 
-    x_limits = ax.get_xlim3d()
-    y_limits = ax.get_ylim3d()
-    z_limits = ax.get_zlim3d()
+    N = round(T * fps)
 
-    x_range = abs(x_limits[1] - x_limits[0])
-    x_middle = np.mean(x_limits)
-    y_range = abs(y_limits[1] - y_limits[0])
-    y_middle = np.mean(y_limits)
-    z_range = abs(z_limits[1] - z_limits[0])
-    z_middle = np.mean(z_limits)
+    if np.isclose(vstart, vstop):  # Hack to enable constant values
+        values = np.full(N, vstart)
+    else:
+        values = np.linspace(vstart, vstop, N)  # Linear sweep
 
-    # The plot bounding box is a sphere in the sense of the infinity
-    # norm, hence I call half the max range the plot radius.
-    plot_radius = 0.5*max([x_range, y_range, z_range])
+    for n in range(N):
+        yield values[n]
 
-    ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
-    ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
-    ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
-
-
-def plot_wing(wing):
-    """Make a plot of a 3D wing"""
-
-    fig = plt.figure(figsize=(10, 10))
-    ax = fig.gca(projection='3d')
-    ax.view_init(azim=-130, elev=25)
-
-    b = wing.geometry.b
-    for y in np.linspace(-b/2, b/2, 21):
-        coords = wing.lower_surface(y, N=50)
-        ax.plot(coords[:, 0], coords[:, 1], -coords[:, 2], c='r', zorder=.9,
-                lw=0.8)
-        coords = wing.upper_surface(y, N=50)
-        ax.plot(coords[:, 0], coords[:, 1], -coords[:, 2], c='b', lw=0.8)
-
-    y = np.linspace(-b/2, b/2, 51)
-    ax.plot(wing.geometry.fx(y), y, -wing.geometry.fz(y), 'g--', lw=0.8)
-
-    set_axes_equal(ax)
-    plt.show()
-
-
-def animated_wing_plotter():
-    # Setup all the configurations I want to step through
-    k = 30
-    dmeds = np.linspace(-30, -15, k)[::-1]
-    smeds = np.linspace(10, 25, k)
-
-    # First, just sweep the dihedral forward and backwards
-    t1 = np.r_[dmeds, dmeds[::-1]]
-    t2 = np.r_[smeds, smeds[::-1]]
-
-    seq1 = np.c_[t1, 10*np.ones_like(t1)]   # Sweep dihedralMed
-    seq2 = np.c_[-15*np.ones_like(t2), t2]  # Sweep sweepMed
-    seq3 = np.c_[t1, t2]                    # Sweep both
-    seq = np.vstack((seq1, seq2, seq3))
-
-    fig = plt.figure(figsize=(10, 10))
-    ax = p3.Axes3D(fig)
-    ax.set_xlim(-4, 4)
-    ax.set_ylim(-4, 4)
-    ax.set_zlim(-6, 4)
-    ax.view_init(azim=-130, elev=25)
-
-    N = 21  # How many airfoil slices (makes 2N lines, for top and bottom)
-    lines = [ax.plot([0], [0], [0], 'r' if n < N else 'b', lw=0.8)[0]
-             for n in range(2*N)]
-
-    def update(frame):
-        print("seq[{}]: {}".format(frame, seq[frame]))
-        dMed, sMed = seq[frame]
-
-        wing = build_elliptical(MAC=2.4, AR=3.9, taper=0.4,
-                                dMed=dMed, sMed=sMed)
-
-        ys = np.linspace(-wing.geometry.b/2, wing.geometry.b/2, N)
-
-        # Update the bottom lines
+    if reverse:
         for n in range(N):
-            coords = wing.lower_surface(ys[n], N=50)
-            coords[:, 2] = -coords[:, 2]
-            lines[n].set_data(coords[:, 0:2].T)
-            lines[n].set_3d_properties(coords[:, 2])
-
-        # Update the top lines
-        for n in range(N):
-            coords = wing.upper_surface(ys[n], N=50)
-            coords[:, 2] = -coords[:, 2]
-            lines[n+N].set_data(coords[:, 0:2].T)
-            lines[n+N].set_3d_properties(coords[:, 2])
-
-        return lines
-
-    ani = animation.FuncAnimation(fig, update, frames=np.arange(len(seq)),
-                                  interval=10)
-    # ani.save('test.mp4', fps=25)
-    plt.show()
+            yield values[::-1][n]
 
 
-def animate_wing_torsion():
-    # Setup all the configurations I want to step through
-    k = 85
-    torsions = np.linspace(-20, 20, k)
-    torsions = np.r_[torsions, torsions[::-1]]
-    torsions = np.r_[torsions, torsions]
-
-    fig = plt.figure(figsize=(10, 10))
-    ax = p3.Axes3D(fig)
-    ax.set_xlim(-5, 5)
-    ax.set_ylim(-5, 5)
-    ax.set_zlim(-6, 4)
-    ax.view_init(azim=-145, elev=0)
-
-    N = 21  # How many airfoil slices (makes 2N lines, for top and bottom)
-    lines = [ax.plot([0], [0], [0], 'r' if n < N else 'b', lw=0.8)[0]
-             for n in range(2*N)]
-
-    def update(frame):
-
-        wing = build_elliptical(MAC=2.2, AR=4.5, taper=0.4,
-                                dMed=-20, sMed=10, torsion=torsions[frame])
-
-        ys = np.linspace(-wing.geometry.b/2, wing.geometry.b/2, N)
-
-        # Update the bottom lines
-        for n in range(N):
-            coords = wing.lower_surface(ys[n], N=50)
-            coords[:, 2] = -coords[:, 2]
-            lines[n].set_data(coords[:, 0:2].T)
-            lines[n].set_3d_properties(coords[:, 2])
-
-        # Update the top lines
-        for n in range(N):
-            coords = wing.upper_surface(ys[n], N=50)
-            coords[:, 2] = -coords[:, 2]
-            lines[n+N].set_data(coords[:, 0:2].T)
-            lines[n+N].set_3d_properties(coords[:, 2])
-
-        return lines
-
-    ani = animation.FuncAnimation(fig, update, frames=np.arange(len(torsions)),
-                                  interval=20)
-    # ani.save('torsion.mp4', fps=50)
-    plt.show()
+def sweep_scalar(name, vstart, vstop, T, reverse=True, fps=60):
+    """Generate a sequence of dictionaries like `{name: scalar}`."""
+    for v in sweep(vstart, vstop, T, reverse, fps):
+        yield {name: f"{v:<4.3g}"}
 
 
-def build_elliptical(MAC, AR, taper, dMed, sMed, dMax=None, sMax=None,
-                     torsion=0, airfoil_geo=None):
-    if dMax is None:
-        print("Using minimum max dihedral")
-        dMax = 2*dMed - 1  # ref page 48 (56)
+# ---------------------------------------------------------------------------
 
-    if sMax is None:
-        print("Using minimum max sweep")
-        sMax = (2*sMed) + 1  # ref page 48 (56)
 
-    # Compute some missing data in reverse
-    c0 = EllipticalWing.MAC_to_c0(MAC, taper)
-    b = EllipticalWing.AR_to_b(c0, AR, taper)
+def sweep_elliptical_lobe1(vstart, vstop, T, reverse=True, fps=60):
+    for am in sweep(vstart, vstop, T, reverse, fps):
+        yield {"yz": f"elliptical_lobe(mean_anhedral={am:<4.3g})"}
 
-    dcg = 0.25  # FIXME: unlisted
-    h0 = 7  # FIXME: unlisted
-    wing_geo = EllipticalWing(
-        b, c0, taper, dMed, dMax, sMed, sMax, torsion=torsion)
 
-    if airfoil_geo is None:
-        airfoil_geo = NACA(2415)
+def sweep_elliptical_lobe2(vstart, vstop, T, reverse=True, fps=60):
+    for am in sweep(vstart, vstop, T, reverse, fps):
+        yield {"yz": f"elliptical_lobe(mean_anhedral={am:<4.3g}, max_anhedral_rate=89)"}
 
-    coefs = LinearCoefficients(5.73, -2, 0.007, -0.05)  # a0, i0, D0, Cm0
 
-    return Wing(wing_geo, Airfoil(coefs, airfoil_geo))
+def sweep_torsion1(s_start, s_stop, peak, exponent, T, fps):
+    for start in sweep(s_start, s_stop, T, fps=fps):
+        yield {
+            "torsion": f"PolynomialTorsion(start={start:<4.3g}, peak={peak:<4.3g}, exponent={exponent:<4.3g})"
+        }
+
+
+def sweep_torsion2(start, peak_start, peak_stop, exponent, T, fps):
+    for peak in sweep(peak_start, peak_stop, T, fps=fps):
+        yield {
+            "torsion": f"PolynomialTorsion(start={start:<4.3g}, peak={peak:<4.3g}, exponent={exponent:<4.3g})"
+        }
+
+
+# ---------------------------------------------------------------------------
+
+
+def foil_generator(fps=60):
+    base_config = {
+        "airfoil": "Airfoil(None, NACA(24018))",
+        "b_flat": "11",
+        "chord_length": "elliptical_chord(root=0.5, tip=0.1)",
+        "r_x": "0.75",
+        "x": "0",
+        "r_yz": "1.00",
+        "yz": "elliptical_lobe(34, 75)",
+        "torsion": "PolynomialTorsion(start=0.8, peak=4, exponent=2)",
+        "intakes": "SimpleIntakes(s_end=0.85, s_upper=-0.04, s_lower=-0.09)",
+    }
+
+    # -----------------------------------------------------------------------
+
+    T1 = 3
+    seq1 = [
+        sweep_scalar("r_x", 0, 1, T1, fps=fps),
+    ]
+
+    T2 = 3
+    seq2 = [
+        sweep_elliptical_lobe1(1e-99, 44, T2, fps=fps),
+    ]
+
+    T3 = 3
+    seq3 = [
+        sweep_elliptical_lobe2(1e-99, 44, T3, fps=fps),
+    ]
+
+    T4 = 3
+    seq4 = [
+        sweep_scalar("r_x", 1, 0, T4, fps=fps),
+        sweep_elliptical_lobe1(1, 44, T4, fps=fps),
+    ]
+
+    T5 = 3
+    seq5 = [
+        sweep_elliptical_lobe1(1e-99, 1e-99, T5, fps=fps),
+        sweep_torsion1(0, 0.8, 10, 2, T5, fps=fps),
+    ]
+
+    T6 = 3
+    seq6 = [
+        # sweep_elliptical_lobe1(1e-99, 1e-99, T6, fps=fps),
+        [{"yz": "FlatYZ()"}] * T6 * fps,
+        [{"r_x": "1"}] * T6 * fps,
+        sweep_torsion2(0.25, 0, 25, 2, T6, fps=fps),
+    ]
+
+    # ----------------------------------------------------------------------
+
+    T7 = 6
+
+    def f7b(T, fps):
+        for p in sweep(0, 1, T, fps=fps):
+            yield {"r_x": f"lambda s: {p:<5.3g} * (1 - s**2)"}
+
+    def f7c(T, fps):
+        for p in sweep(0, 1, T, fps=fps):
+            yield {"r_x": f"lambda s: {p:<5.3g} * s**2"}
+
+    seq7a = [
+        [{"yz": "FlatYZ()"}] * T7 * fps,
+        sweep_scalar("r_x", 0, 1, T7, fps=fps),
+    ]
+    seq7b = [
+        [{"yz": "FlatYZ()"}] * T7 * fps,
+        f7b(T7, fps),
+    ]
+    seq7c = [
+        [{"yz": "FlatYZ()"}] * T7 * fps,
+        f7c(T7, fps),
+    ]
+    seq7d = [
+        [{"yz": "FlatYZ()"}] * T7 * fps,
+        f7b(T7, fps),
+        sweep_torsion2(0, 0, 25, 2, T7, fps=fps),
+    ]
+    seq7e = [
+        [{"yz": "FlatYZ()"}] * T7 * fps,
+        f7c(T7, fps),
+        sweep_torsion2(0, 0, 25, 2, T7, fps=fps),
+    ]
+    seq7f = [
+        f7b(T7 * 2, fps),
+        sweep_torsion2(0, 0, 45, 1, T7 * 2, fps=fps),
+        sweep_elliptical_lobe1(1, 44, T7 * 2, fps=fps),
+    ]
+
+    # ----------------------------------------------------------------------
+
+    def f8(T, fps):
+        for p in sweep(25, -25, T, fps=fps):
+            yield {"torsion": f"PolynomialTorsion(start=0, peak={p:<4.3g}, exponent=2)"}
+
+    T8 = 6
+    seq8 = [
+        [{"yz": "FlatYZ()"}] * T8 * fps,
+        sweep_scalar("r_x", 0, 1, T8, fps=fps),
+        # [{"torsion": f"PolynomialTorsion(start=0, peak=25, exponent=2)"}] * T8 * fps,
+        f8(T8, fps),
+    ]
+
+    # ----------------------------------------------------------------------
+
+    sequences = [
+        # [seq1, T1],
+        # [seq2, T2],
+        # [seq3, T3],
+        # [seq4, T4],
+        # [seq5, T5],
+        # [seq6, T6],
+        # [seq7a, T7],
+        # [seq7b, T7],
+        # [seq7c, T7],
+        # [seq7d, T7],
+        [seq7e, T7],
+        [seq7f, T7 * 2],
+        # [seq8, T8],
+    ]
+
+    # -----------------------------------------------------------------------
+
+    n = 0  # Current frame
+    N = sum(s[1] for s in sequences) * fps  # Total number of frames
+    for seq, _T in sequences:  # Each sequence is a set of generators
+        for config_set in zip(*seq):  # For each generated set of configurations
+            modifications = dict(
+                ChainMap(*config_set)
+            )  # Combine the set into a single dict
+            print(f"\rYielding frame: {n} / {N}", end="")
+            # print("DEBUG> modifications:", modifications)
+            config = base_config.copy()  # Baseline configuration strings
+            config.update(modifications)  # Modified configuration strings
+            params = {}  # The `exec`uted result
+            for k, v in config.items():
+                exec(f"params['{k}'] = {v}")
+            yield config, gsim.foil.FoilGeometry(**params)
+            n += 1
+    print()
+
+
+def update(config_and_foil, axes):
+    """Update the plot."""
+    # print(f"Entering `update`, frames={frames}")
+
+    config, foil = config_and_foil
+
+    # First, clear any old artists:
+    while axes[0].texts:
+        a = axes[0].texts[0]
+        a.remove()
+    while axes[1].lines:
+        a = axes[1].lines[0]
+        a.remove()
+    while axes[1].collections:
+        a = axes[1].collections[0]
+        a.remove()
+
+    code = """\
+from pfh.glidersim.airfoil import Airfoil, NACA
+from pfh.glidersim.foil import (
+    elliptical_lobe,
+    elliptical_chord,
+    PolynomialTorsion,
+    SimpleIntakes,
+)
+
+foil = pfh.glidersim.foil.FoilGeometry(
+  """
+
+    maxlen = max(len(k) for k in config.keys())  # For aligning the "="
+    code += "\n  ".join(f"{k:>{maxlen}} = {v}" for k, v in config.items())
+    code += "\n)\n\npfh.glidersim.plots.plot_parafoil_geo(foil)"
+    code_text = axes[0].text(
+        0,
+        0.5,
+        code,
+        fontsize=8,
+        family="monospace",
+        transform=axes[0].transAxes,
+        verticalalignment="center",
+    )
+    foil_artists = gsim.plots.plot_parafoil_geo(
+        foil, N_sections=51, ax=axes[1], surface_lw=0.25,
+    )
+
+    return (code_text, *foil_artists)
 
 
 if __name__ == "__main__":
-    # plot_airfoil_geo(NACA(2412))
-    # plot_airfoil_geo(NACA(4412))
-    # plot_airfoil_geo(NACA(2415))
+    save = False
+    # save = True
 
-    # animated_wing_plotter()
-    animate_wing_torsion()
+    dpi = 400 if save else 100
+    fps = 60 if save else 10
+    fig = plt.figure(figsize=(16, 9), dpi=dpi, constrained_layout=False)
+    gs = fig.add_gridspec(1, 2, width_ratios=[1, 2], wspace=0, hspace=0)
+    gs.tight_layout(fig, pad=1, h_pad=0, w_pad=0)
 
-    print("\n\n-----\nTrying to produce the 'standard wing' from page 89 (97)")
-    wing = build_elliptical(
-        MAC=2.4, AR=3.9, taper=0.4, dMed=-20, sMed=10)
-    plot_wing(wing)
+    # Setup the text region
+    ax1 = fig.add_subplot(gs[0])
+    ax1.spines["left"].set_visible(False)
+    ax1.spines["top"].set_visible(False)
+    ax1.spines["right"].set_visible(False)
+    ax1.spines["bottom"].set_visible(False)
+    ax1.xaxis.set_ticks([])
+    ax1.xaxis.set_ticklabels("")
+    ax1.yaxis.set_ticks([])
+    ax1.yaxis.set_ticklabels("")
 
-    print("\n\nPausing\n\n")
-    embed()
+    # Setup the 3D wing region
+    ax2 = fig.add_subplot(gs[1], projection="3d")
+    ax2.invert_yaxis()
+    ax2.invert_zaxis()
+    ax2.view_init(azim=-120, elev=20)
+    ax2.set_xlabel("x [m]")
+    ax2.set_ylabel("y [m]")
+    ax2.set_zlabel("z [m]")
 
-    # Demonstrate how dMax controls "smoothness"
-    print("With dMax=-70")
-    wing = build_elliptical(
-        MAC=2.4, AR=3.9, taper=0.4, dMed=-20, dMax=-55, sMed=10)
-    plot_wing(wing)
-    print("With dMax=-85")
-    wing = build_elliptical(
-        MAC=2.4, AR=3.9, taper=0.4, dMed=-20, dMax=-85, sMed=10)
-    plot_wing(wing)
+    axes = [ax1, ax2]
+    fig.tight_layout()
 
-    # Demonstrate how sMax controls ???
-    print("sMed=10, sMax=50")
-    wing = build_elliptical(
-        MAC=2.4, AR=3.9, taper=0.4, dMed=-20, sMed=10, sMax=50)
-    plot_wing(wing)
-    print("sMed=10, sMax=85")
-    wing = build_elliptical(
-        MAC=2.4, AR=3.9, taper=0.4, dMed=-20, sMed=10, sMax=85)
-    plot_wing(wing)
-    print("sMed=30, sMax=85")
-    wing = build_elliptical(
-        MAC=2.4, AR=3.9, taper=0.4, dMed=-20, sMed=30, sMax=85)
-    plot_wing(wing)
+    ani = animation.FuncAnimation(
+        fig,
+        update,
+        frames=foil_generator(fps=fps),  # Produces (config, FoilGeometry)
+        fargs=(axes,),
+        repeat=False,
+        interval=20,
+        save_count=999999,
+    )
 
-    # Torsion
-    print("With 15 degree torsion")
-    wing = build_elliptical(
-        MAC=2.4, AR=3.9, taper=0.4, dMed=-20, sMed=10, torsion=15)
-    plot_wing(wing)
-    print("With -15 degree torsion")
-    wing = build_elliptical(
-        MAC=2.4, AR=3.9, taper=0.4, dMed=-20, sMed=10, torsion=-15)
-    plot_wing(wing)
+    if save:
+        # progress_callback = lambda i, n: print(f"\rSaving frame {i}", end="")  # noqa: E731
+        progress_callback = None
 
-    # More wings from page 89
-    wing = build_elliptical(MAC=2.4, AR=3.9, taper=0.4, dMed=-35, sMed=10)
-    plot_wing(wing)
+        writer = animation.FFMpegWriter(
+            fps=fps,
+            # bitrate=10_000,
+            extra_args=[
+                "-vf",
+                "scale='-1:min(1440,iw)'",  # Scale to 1440px horizontally
+                "-sws_flags",
+                "lanczos",
+            ],
+        )
+        ani.save(
+            "/home/peter/animated.mp4",
+            progress_callback=progress_callback,
+            dpi=dpi,
+            writer=writer,
+        )
+    else:
+        plt.show()
 
-    wing = build_elliptical(MAC=2.4, AR=3.9, taper=0.4, dMed=-20, sMed=25)
-    plot_wing(wing)
+    print("\nDone.")
 
-    wing = build_elliptical(MAC=2.2, AR=4.9, taper=0.4, dMed=-20, sMed=10)
-    plot_wing(wing)
-
-    wing = build_elliptical(MAC=2.3, AR=4.0, taper=0.6, dMed=-20, sMed=10)
-    plot_wing(wing)
+    # embed()
