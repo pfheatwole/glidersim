92b393f9328f3ef56a00ab0e8afd0e5d8fb39ef8
Peter Heatwole <pheatwole@gmail.com>
2019-12-31 14:10:18 -0800

    Refactor animation sequences into functions

--- scripts/animate_foils.py
+++ scripts/animate_foils.py
@@ -1,26 +1,27 @@
 from collections import ChainMap
 
+from IPython import embed  # noqa: F401
+
 import matplotlib.pyplot as plt
 from matplotlib import animation
+
 from mpl_toolkits.mplot3d import Axes3D  # noqa: F401; for `projection='3d'`
 
 import numpy as np
-from numpy import sin, cos, sqrt, arcsin, arctan  # noqa: F401
-
-from IPython import embed  # noqa: F401
 
 import pfh.glidersim as gsim
-from pfh.glidersim.airfoil import Airfoil, NACA
-from pfh.glidersim.foil import (
-    elliptical_lobe,
-    elliptical_chord,
+from pfh.glidersim.airfoil import Airfoil, NACA  # noqa: F401
+from pfh.glidersim.foil import (  # noqa: F401
     FlatYZ,
-    PolynomialTorsion,
-    SimpleIntakes,
+    FoilGeometry,
+    PolynomialTorsion as PT,
+    SimpleIntakes as SI,
+    elliptical_chord,
+    elliptical_lobe,
 )
 
 
-def sweep(vstart, vstop, T, reverse=True, fps=60):
+def sweep(vstart, vstop, T, fps, reverse=True):
     """
     Generate a sequence of scalars.
 
@@ -52,148 +53,216 @@ def sweep(vstart, vstop, T, reverse=True, fps=60):
             yield values[::-1][n]
 
 
-def sweep_scalar(name, vstart, vstop, T, reverse=True, fps=60):
+def sweep_scalar(name, vstart, vstop, T, fps, reverse=True):
     """Generate a sequence of dictionaries like `{name: scalar}`."""
-    for v in sweep(vstart, vstop, T, reverse, fps):
+    for v in sweep(vstart, vstop, T, fps, reverse):
         yield {name: f"{v:<4.3g}"}
 
 
 # ---------------------------------------------------------------------------
 
 
-def sweep_elliptical_lobe1(vstart, vstop, T, reverse=True, fps=60):
-    for am in sweep(vstart, vstop, T, reverse, fps):
-        yield {"yz": f"elliptical_lobe(mean_anhedral={am:<4.3g})"}
+def SEQ_sweep_chord_lengths(T, fps):
+    """Sweep chord_length over a rectangular wing."""
+    seq = [
+        sweep_scalar("chord_length", .1, .5, T=T, fps=fps),
+    ]
+    return seq, T
 
 
-def sweep_elliptical_lobe2(vstart, vstop, T, reverse=True, fps=60):
-    for am in sweep(vstart, vstop, T, reverse, fps):
-        yield {"yz": f"elliptical_lobe(mean_anhedral={am:<4.3g}, max_anhedral_rate=89)"}
+def SEQ_sweep_linear_chord_ratios(T, fps):
+    """Sweep the central chord while hold the tip constant."""
+
+    def f(vmin, vmax, tip, T, fps):
+        for c0 in sweep(vmin, vmax, T, fps):
+            m = c0 - tip
+            yield {"chord_length": f"lambda s: {c0:>4.3g} - {m:4.3g} * abs(s)"}
+
+    seq = [
+        f(.1, .5, .1, T, fps),
+    ]
+
+    return seq, T
 
 
-def sweep_torsion1(s_start, s_stop, peak, exponent, T, fps):
-    for start in sweep(s_start, s_stop, T, fps=fps):
-        yield {
-            "torsion": f"PolynomialTorsion(start={start:<4.3g}, peak={peak:<4.3g}, exponent={exponent:<4.3g})"
-        }
+def SEQ_sweep_elliptical_chord_ratios(T, fps):
+    """Sweep the central chord while hold the tip constant."""
+
+    def f(vmin, vmax, tip, T, fps):
+        for c0 in sweep(vmin, vmax, T, fps):
+            yield {"chord_length": f"elliptical_chord({c0:4.3g}, {tip:4.3g})"}
+
+    seq = [
+        f(.11, .7, .1, T, fps),
+    ]
+
+    return seq, T
 
 
-def sweep_torsion2(start, peak_start, peak_stop, exponent, T, fps):
-    for peak in sweep(peak_start, peak_stop, T, fps=fps):
-        yield {
-            "torsion": f"PolynomialTorsion(start={start:<4.3g}, peak={peak:<4.3g}, exponent={exponent:<4.3g})"
-        }
+def SEQS_sweep_xrx_rectangle(T, fps):
+    def f1(T, fps):
+        for p in sweep(0, 0.5, T, fps, reverse=False):
+            yield {"x": f"lambda s: {p:>#.3f} * (1 - abs(s))"}
 
+    def f2(T, fps):
+        for p in sweep(1, 2, T, fps, reverse=False):
+            yield {"x": f"lambda s: 0.5 * (1 - abs(s)**{p:#<.3f})"}
 
-def compose_sequences(fps):
-    T1 = 3
+    def f3(T, fps):
+        for p in sweep(0.5, 0, T, fps, reverse=False):
+            yield {"x": f"lambda s: {p:>#.3f} * (1 - s**2)"}
+
     seq1 = [
-        sweep_scalar("r_x", 0, 1, T1, fps=fps),
+        [{"x": "0"}] * T * fps,
+        sweep_scalar("r_x", 0, 1, T, fps),
     ]
 
-    T2 = 3
     seq2 = [
-        sweep_elliptical_lobe1(1e-99, 44, T2, fps=fps),
+        [{"r_x": "0.5"}] * T * fps,
+        f1(T, fps),
     ]
-
-    T3 = 3
     seq3 = [
-        sweep_elliptical_lobe2(1e-99, 44, T3, fps=fps),
+        [{"r_x": "0.5"}] * T * fps,
+        f2(T, fps),
     ]
-
-    T4 = 3
     seq4 = [
-        sweep_scalar("r_x", 1, 0, T4, fps=fps),
-        sweep_elliptical_lobe1(1, 44, T4, fps=fps),
+        [{"r_x": "0.5"}] * T * fps,
+        f3(T, fps),
+    ]
+    return (seq1, T), (seq2, T), (seq3, T), (seq4, T)
+
+
+def SEQS_sweep_xrx_triangle(T, fps):
+    def f1(T, fps):
+        for p in sweep(0, 0.5, T, fps, reverse=False):
+            yield {"x": f"lambda s: {p:>#.3f} * (1 - abs(s))"}
+
+    def f2(T, fps):
+        for p in sweep(1, 2, T, fps, reverse=False):
+            yield {"x": f"lambda s: 0.5 * (1 - abs(s)**{p:<#.3f})"}
+
+    def f3(T, fps):
+        for p in sweep(0.5, 0, T, fps, reverse=False):
+            yield {"x": f"lambda s: {p:>#.3f} * (1 - s**2)"}
+
+    seq1 = [
+        [{"chord_length": "lambda s: 0.5 * (1 - abs(s))"}] * T * fps,
+        [{"x": "0"}] * T * fps,
+        sweep_scalar("r_x", 0, 1, T, fps),
     ]
 
-    T5 = 3
-    seq5 = [
-        sweep_elliptical_lobe1(1e-99, 1e-99, T5, fps=fps),
-        sweep_torsion1(0, 0.8, 10, 2, T5, fps=fps),
+    seq2 = [
+        [{"chord_length": "lambda s: 0.5 * (1 - abs(s))"}] * T * fps,
+        [{"r_x": "0.5"}] * T * fps,
+        f1(T, fps),
+    ]
+    seq3 = [
+        [{"chord_length": "lambda s: 0.5 * (1 - abs(s))"}] * T * fps,
+        [{"r_x": "0.5"}] * T * fps,
+        f2(T, fps),
+    ]
+    seq4 = [
+        [{"chord_length": "lambda s: 0.5 * (1 - abs(s))"}] * T * fps,
+        [{"r_x": "0.5"}] * T * fps,
+        f3(T, fps),
+    ]
+    return (seq1, T), (seq2, T), (seq3, T), (seq4, T)
+
+
+def SEQS_sweep_xrx_elliptical(T, fps):
+    def f1(T, fps):
+        for p in sweep(0, 0.5, T, fps, reverse=False):
+            yield {"x": f"lambda s: {p:>#.3f} * (1 - abs(s))"}
+
+    def f2(T, fps):
+        for p in sweep(1, 2, T, fps, reverse=False):
+            yield {"x": f"lambda s: 0.5 * (1 - abs(s)**{p:<#.3f})"}
+
+    def f3(T, fps):
+        for p in sweep(0.5, 0, T, fps, reverse=False):
+            yield {"x": f"lambda s: {p:>#.3f} * (1 - s**2)"}
+
+    seq1 = [
+        [{"chord_length": "elliptical_chord(root=0.5, tip=0.1)"}] * T * fps,
+        [{"x": "0"}] * T * fps,
+        sweep_scalar("r_x", 0, 1, T, fps),
     ]
 
-    T6 = 3
-    seq6 = [
-        # sweep_elliptical_lobe1(1e-99, 1e-99, T6, fps=fps),
-        [{"yz": "FlatYZ()"}] * T6 * fps,
-        [{"r_x": "1"}] * T6 * fps,
-        sweep_torsion2(0.25, 0, 25, 2, T6, fps=fps),
+    seq2 = [
+        [{"chord_length": "elliptical_chord(root=0.5, tip=0.1)"}] * T * fps,
+        [{"r_x": "0.5"}] * T * fps,
+        f1(T, fps),
+    ]
+    seq3 = [
+        [{"chord_length": "elliptical_chord(root=0.5, tip=0.1)"}] * T * fps,
+        [{"r_x": "0.5"}] * T * fps,
+        f2(T, fps),
+    ]
+    seq4 = [
+        [{"chord_length": "elliptical_chord(root=0.5, tip=0.1)"}] * T * fps,
+        [{"r_x": "0.5"}] * T * fps,
+        f3(T, fps),
+    ]
+    return (seq1, T), (seq2, T), (seq3, T), (seq4, T)
+
+
+def SEQS_sweep_xrx_elliptical_arched(T, fps):
+    def f(T, fps):
+        for p in sweep(0, .25, T, fps):
+            yield {"x": f"lambda s: {p:>#.3f} * (1 - (s**2))"}
+
+    seq1 = [
+        [{"chord_length": "elliptical_chord(root=0.5, tip=0.1)"}] * T * fps,
+        [{"x": "0"}] * T * fps,
+        sweep_scalar("r_x", 0, 1, T, fps),
+        [{"yz": "elliptical_lobe(34, 75)"}] * T * fps,
     ]
 
-    # ----------------------------------------------------------------------
+    seq2 = [
+        [{"chord_length": "elliptical_chord(root=0.5, tip=0.1)"}] * T * fps,
+        [{"r_x": "0.5"}] * T * fps,
+        f(T, fps),
+        [{"yz": "elliptical_lobe(34, 75)"}] * T * fps,
+    ]
+    return (seq1, T), (seq2, T)
 
-    T7 = 6
 
-    def f7b(T, fps):
-        for p in sweep(0, 1, T, fps=fps):
-            yield {"r_x": f"lambda s: {p:<5.3g} * (1 - s**2)"}
+def SEQS_sweep_elliptical_anhedral(T, fps):
+    def f1(vstart, vstop, T, fps, reverse=True):
+        for ma in sweep(vstart, vstop, T, fps, reverse):
+            yield {"yz": f"elliptical_lobe({ma:<#5.2f})"}
 
-    def f7c(T, fps):
-        for p in sweep(0, 1, T, fps=fps):
-            yield {"r_x": f"lambda s: {p:<5.3g} * s**2"}
+    def f2(vstart, vstop, T, fps, reverse=True):
+        ma = vstart
+        for mar in sweep(vstart * 2 + 1, vstop, T, fps, reverse):
+            yield {"yz": f"elliptical_lobe({ma:<#5.2f}, {mar:<#5.2f})"}
 
-    seq7a = [
-        [{"yz": "FlatYZ()"}] * T7 * fps,
-        sweep_scalar("r_x", 0, 1, T7, fps=fps),
+    seq1 = [
+        [{"chord_length": "elliptical_chord(0.5, 0.1)"}] * T * fps,
+        f1(1, 44, T, fps),
     ]
-    seq7b = [
-        [{"yz": "FlatYZ()"}] * T7 * fps,
-        f7b(T7, fps),
-    ]
-    seq7c = [
-        [{"yz": "FlatYZ()"}] * T7 * fps,
-        f7c(T7, fps),
-    ]
-    seq7d = [
-        [{"yz": "FlatYZ()"}] * T7 * fps,
-        f7b(T7, fps),
-        sweep_torsion2(0, 0, 25, 2, T7, fps=fps),
-    ]
-    seq7e = [
-        [{"yz": "FlatYZ()"}] * T7 * fps,
-        f7c(T7, fps),
-        sweep_torsion2(0, 0, 25, 2, T7, fps=fps),
-    ]
-    seq7f = [
-        f7b(T7 * 2, fps),
-        sweep_torsion2(0, 0, 45, 1, T7 * 2, fps=fps),
-        sweep_elliptical_lobe1(1, 44, T7 * 2, fps=fps),
+    seq2 = [
+        [{"chord_length": "elliptical_chord(0.5, 0.1)"}] * T * fps,
+        f2(20, 89, T, fps),
     ]
 
-    # ----------------------------------------------------------------------
+    return (seq1, T), (seq2, T)
 
-    def f8(T, fps):
-        for p in sweep(25, -25, T, fps=fps):
-            yield {"torsion": f"PolynomialTorsion(start=0, peak={p:<4.3g}, exponent=2)"}
 
-    T8 = 6
-    seq8 = [
-        [{"yz": "FlatYZ()"}] * T8 * fps,
-        sweep_scalar("r_x", 0, 1, T8, fps=fps),
-        # [{"torsion": f"PolynomialTorsion(start=0, peak=25, exponent=2)"}] * T8 * fps,
-        f8(T8, fps),
-    ]
+def SEQS_sweep_torsion(T, fps):
+    def f1(start, peak_start, peak_stop, exponent, T, fps):
+        for peak in sweep(peak_start, peak_stop, T, fps=fps, reverse=False):
+            yield {"torsion": f"PT(start={start}, peak={peak:<#6.3f}, exponent={exponent})"}
 
-    # ----------------------------------------------------------------------
+    def f2(s_start, s_stop, peak, exponent, T, fps):
+        for start in sweep(s_start, s_stop, T, fps=fps):
+            yield {"torsion": f"PT(start={start:<#6.3f}, peak={peak}, exponent={exponent})"}
 
-    sequences = [
-        [seq1, T1],
-        [seq2, T2],
-        [seq3, T3],
-        [seq4, T4],
-        # [seq5, T5],
-        # [seq6, T6],
-        # [seq7a, T7],
-        # [seq7b, T7],
-        # [seq7c, T7],
-        # [seq7d, T7],
-        # [seq7e, T7],
-        # [seq7f, T7 * 2],
-        [seq8, T8],
-    ]
+    seq1 = [f1(0, 0, 25, 2, T, fps)]
+    seq2 = [f2(0, 0.8, 25, 2, T, fps)]
+    seq3 = [f1(0, 25, 0, 2, T, fps)]
 
-    return sequences
+    return (seq1, T), (seq2, T), (seq3, T)
 
 
 # ---------------------------------------------------------------------------
@@ -202,7 +271,7 @@ def compose_sequences(fps):
 def setup_figure(dpi):
     fig = plt.figure(figsize=(16, 9), dpi=dpi, constrained_layout=False)
     gs = fig.add_gridspec(1, 2, width_ratios=[1, 2], wspace=0, hspace=0)
-    gs.tight_layout(fig, pad=1, h_pad=0, w_pad=0)
+    # gs.tight_layout(fig, pad=100, h_pad=0, w_pad=0)  # FIXME: useless?
 
     # Setup the text region
     ax1 = fig.add_subplot(gs[0])
@@ -272,11 +341,13 @@ from pfh.glidersim.airfoil import Airfoil, NACA
 from pfh.glidersim.foil import (
     elliptical_lobe,
     elliptical_chord,
-    PolynomialTorsion,
-    SimpleIntakes,
+    FoilGeometry,
+    FlatYZ,
+    PolynomialTorsion as PT,
+    SimpleIntakes as SI,
 )
 
-foil = pfh.glidersim.foil.FoilGeometry(
+foil = FoilGeometry(
   """
 
     maxlen = max(len(k) for k in config.keys())  # For aligning the "="
@@ -298,27 +369,38 @@ foil = pfh.glidersim.foil.FoilGeometry(
 
 if __name__ == "__main__":
     outfile = '/home/peter/animated.mp4'
-    outfile = None  # Switch to live plot (disables movie output)
+    outfile = None  # Disable movie output (use a live plot)
 
     # Use high quality output for movies
-    dpi = 400 if outfile else 100
+    dpi = 200 if outfile else 150
     fps = 60 if outfile else 10
 
     # Each sequence modifies this baseline configuration
     base_config = {
         "airfoil": "Airfoil(None, NACA(24018))",
         "b_flat": "10",
-        # "chord_length": "elliptical_chord(root=0.5, tip=0.1)",
-        "chord_length": "0.2",
+        "chord_length": "0.3",
         "r_x": "0.5",
         "x": "0",
         "r_yz": "1.00",
         "yz": "FlatYZ()",
         "torsion": "0",
         "intakes": "None",
+        "center": "False",
     }
 
-    sequences = compose_sequences(fps)
+    sequences = [
+        SEQ_sweep_chord_lengths(3, fps),
+        SEQ_sweep_linear_chord_ratios(3, fps),
+        SEQ_sweep_elliptical_chord_ratios(3, fps),
+        *SEQS_sweep_xrx_rectangle(3, fps),
+        *SEQS_sweep_xrx_triangle(3, fps),
+        *SEQS_sweep_xrx_elliptical(3, fps),
+        *SEQS_sweep_xrx_elliptical_arched(3, fps),
+        *SEQS_sweep_elliptical_anhedral(3, fps),
+        *SEQS_sweep_torsion(3, fps),
+    ]
+
     frames = foil_generator(base_config, sequences, fps)
 
     fig, axes = setup_figure(dpi)
@@ -344,12 +426,8 @@ if __name__ == "__main__":
             ],
         )
 
-        # progress_callback = lambda i, n: print(f"\rSaving frame {i}", end="")  # noqa: E731
-        progress_callback = None  # FIXME: not helpful if you dont know the total number of frames to be rendered
-
         ani.save(
             outfile,
-            progress_callback=progress_callback,
             dpi=dpi,
             writer=writer,
         )
