229ad9c362323c52b7761216cef02a0db8faa2cd
Peter Heatwole <pheatwole@gmail.com>
2020-01-18 13:05:52 -0800

    Add demo: interpolating surface points

--- scripts/animate_airfoil_interpolation.py
+++ scripts/animate_airfoil_interpolation.py
@@ -16,51 +16,73 @@ airfoil2 = gsim.airfoil.NACA(7911, convention='perpendicular')
 gsim.plots.plot_airfoil_geo(airfoil1)
 gsim.plots.plot_airfoil_geo(airfoil2)
 
-fig, ax = plt.subplots()
-ax.set_xlim(-0.1, 1.1)
-ax.set_ylim(-0.4, 0.4)
+fig, axes = plt.subplots(2)
+axes[0].set_xlim(-0.1, 1.1)
+axes[0].set_ylim(-0.4, 0.4)
+axes[1].set_xlim(-0.1, 1.1)
+axes[1].set_ylim(-0.4, 0.4)
 # ax.set_aspect("equal")
 # ax.margins(x=0.1, y=0.40)
-ul = plt.plot([], [], c="b", lw=0.75)[0]
-cl = plt.plot([], [], c="k", lw=0.75, linestyle="--")[0]
-ll = plt.plot([], [], c="r", lw=0.75)[0]
-ax.grid(True)
+ul1 = axes[0].plot([], [], c="b", lw=0.75)[0]
+cl1 = axes[0].plot([], [], c="k", lw=0.75, linestyle="--")[0]
+ll1 = axes[0].plot([], [], c="r", lw=0.75)[0]
+ul2 = axes[1].plot([], [], c="k", lw=0.55)[0]
+# cl2 = axes[1].plot([], [], c="k", lw=0.75, linestyle="--")[0]
+ll2 = axes[1].plot([], [], c="k", lw=0.55)[0]
+axes[0].grid(True)
+axes[1].grid(True)
+axes[0].set_aspect("equal")
+axes[1].set_aspect("equal")
 N_points = 300
 N_steps = 60 * 3
 
 pc = (1 - np.cos(np.linspace(0, np.pi, N_points))) / 2  # `0 <= x <= 1`
-c1 = airfoil1.camber_curve(pc)
-c2 = airfoil2.camber_curve(pc)
+xyu1 = airfoil1.surface_curve(pc)
+xyu2 = airfoil2.surface_curve(pc)
+xyl1 = airfoil1.surface_curve(-pc)
+xyl2 = airfoil2.surface_curve(-pc)
+xyc1 = airfoil1.camber_curve(pc)
+xyc2 = airfoil2.camber_curve(pc)
 t1 = airfoil1.thickness(pc)
 t2 = airfoil2.thickness(pc)
 
 
 def setup():
-    ul.set_data([], [])
-    cl.set_data([], [])
-    ll.set_data([], [])
-    return ul, cl, ll
+    ul1.set_data([], [])
+    cl1.set_data([], [])
+    ll1.set_data([], [])
+    ul2.set_data([], [])
+    # cl2.set_data([], [])
+    ll2.set_data([], [])
+    return ul1, cl1, ll1, ul2, ll2
 
 
 def update(frame):
-    print(f"\r{frame}", end="")
     if frame <= N_steps:
         p = frame / N_steps  # Counting up
     else:
         p = 1 - (frame - N_steps) / N_steps  # Counting down
+    print(f"\r{frame}", end="")
 
-    cc = c1 + p * (c2 - c1)
-    cx, cy = cc.T
+
+    # First method: interpolate the camber and thickness
+    xyc = xyc1 + p * (xyc2 - xyc1)  # The interpolated camber curve
+    cx, cy = xyc.T
     t = t1 + p * (t2 - t1)
+    ul1.set_data(cx, cy + t / 2)
+    cl1.set_data(cx, cy)
+    ll1.set_data(cx, cy - t / 2)
 
-    ul.set_data(cx, cy + t / 2)
-    cl.set_data(cx, cy)
-    ll.set_data(cx, cy - t / 2)
+    # Second method: interpolate points on the surfaces
+    xyu = xyu1 + p * (xyu2 - xyu1)
+    xyl = xyl1 + p * (xyl2 - xyl1)
+    ul2.set_data(xyu.T[0], xyu.T[1])
+    ll2.set_data(xyl.T[0], xyl.T[1])
 
-    ax.set_aspect("equal")
+    return (ul1, cl1, ll1, ul2, ll2)
 
-    return (ul, cl, ll)
 
+# embed()
 
 ani = animation.FuncAnimation(
     fig, update, frames=N_steps * 2, init_func=setup, blit=True, interval=20,
