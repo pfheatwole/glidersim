3e76466a4dece85f00c664a332e52a2301d2081a
Peter Heatwole <pheatwole@gmail.com>
2020-02-09 15:41:39 -0800

    Tiny simplification

--- scripts/belloc.py
+++ scripts/belloc.py
@@ -218,7 +218,7 @@ for kb, beta_deg in enumerate(betas):
     Fs[beta_deg], Ms[beta_deg], solutions[beta_deg] = [], [], []
 
     alphas_up = np.deg2rad(np.linspace(2, 22, 75))
-    alphas_down = np.deg2rad(np.linspace(-5, 2, 30, endpoint=False))[::-1]
+    alphas_down = np.deg2rad(np.linspace(2, -5, 30))[1:]
 
     # First, going down
     ref = None
