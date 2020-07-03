ff269b394d9b6977237876db3cc5837a8ef3c235
Peter Heatwole <pheatwole@gmail.com>
2020-07-03 11:20:46 -0700

    Fix switchover to LineGeometry

--- scripts/test_sim.py
+++ scripts/test_sim.py
@@ -688,9 +688,9 @@ def main():
 
     K = len(times)
     if np.isscalar(delta_a):
-        LE0 = model.glider.wing.c_0 * model.glider.wing.canopy_origin(delta_a)
+        LE0 = model.glider.wing.c_0 * model.glider.wing.lines.canopy_origin(delta_a)
     else:
-        LE0 = model.glider.wing.c_0 * model.glider.wing.canopy_origin(delta_a(times))
+        LE0 = model.glider.wing.c_0 * model.glider.wing.lines.canopy_origin(delta_a(times))
     q_e2b = path["q_b2e"] * [1, -1, -1, -1]  # Applies C_ned/frd
     r_LE0 = path["r_R2O"] + quaternion.apply_quaternion_rotation(q_e2b, LE0)
     v_LE0 = path["v_R2e"] + quaternion.apply_quaternion_rotation(
