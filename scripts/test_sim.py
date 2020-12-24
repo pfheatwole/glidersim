ea2176f174cb496783f2e82abe5852a420f09e1d
Peter Heatwole <pfheatwole@gmail.com>
2020-12-23 17:08:11 -0800

    Refactor simulator into its own module

--- scripts/test_sim.py
+++ scripts/test_sim.py
@@ -7,41 +7,16 @@ from mpl_toolkits.mplot3d import Axes3D  # noqa: F401
 
 import numpy as np
 
-import scipy.integrate
 from scipy.interpolate import interp1d
 
 import hook3
 import pfh.glidersim as gsim
 from pfh.glidersim import orientation
+from pfh.glidersim import simulator
 
 
-def linear_control(pairs):
-    """
-    Helper funtion to build linear interpolators for control inputs.
-
-    The input is a sequence of tuples encoding  `(duration, value)`. An initial
-    value can be set with a leading `(0, initial_value)` tuple. To "hold" a
-    value, use `None` to repeat the previous value.
-
-    For example, to ramp from 0 to 0.5 over the initial 15 seconds, then
-    transition to 0.75 over a period of 2 seconds, hold for 10 seconds, then
-    decrease to 0 over 10 seconds:
-
-        pairs = [(15, 0.5), (2, 0.75), (10, None), (10, 0)]
-
-    Parameters
-    ----------
-    pairs : list of 2-tuples of float
-        Each tuple is (duration, value).
-    """
-    durations = np.array([t[0] for t in pairs])
-    values = [t[1] for t in pairs]
-    assert all(durations >= 0)
-    for n, v in enumerate(values):  # Use `None` for "hold previous value"
-        values[n] = v if v is not None else values[n - 1]
-    times = np.cumsum(durations)
-    c = interp1d(times, values, fill_value=(values[0], values[-1]), bounds_error=False)
-    return c
+# ---------------------------------------------------------------------------
+# Simple wind "models"
 
 
 class CircularThermal:
@@ -113,379 +88,40 @@ class LateralGust:
         return wind
 
 
-class Dynamics6a:
-    """Defines the state dynamics for a 6 DoF paraglider model."""
-
-    # FIXME: I dislike this notation. It confuses the reference frames with
-    #        the coordinate systems embedded in those frames.
-    state_dtype = [
-        ("q_b2e", float, (4,)),  # Orientation: body/earth
-        ("omega_b2e", float, (3,)),  # Angular velocity of the body in body frd
-        ("r_R2O", float, (3,)),  # The position of `R` in ned
-        ("v_R2e", float, (3,)),  # The velocity of `R` in ned
-    ]
-
-    def __init__(
-        self, glider, rho_air, delta_a=0, delta_bl=0, delta_br=0, delta_w=0, v_W2e=None,
-    ):
-        self.glider = glider
-
-        if callable(rho_air):
-            self.rho_air = rho_air
-        elif np.isscalar(rho_air):
-            self.rho_air = lambda t: rho_air
-        else:
-            raise ValueError("`rho_air` must be a scalar or callable")
-
-        if callable(delta_a):
-            self.delta_a = delta_a
-        elif np.isscalar(delta_a):
-            self.delta_a = lambda t: delta_a
-        else:
-            raise ValueError("`delta_a` must be a scalar or callable")
-
-        if callable(delta_bl):
-            self.delta_bl = delta_bl
-        elif np.isscalar(delta_bl):
-            self.delta_bl = lambda t: delta_bl
-        else:
-            raise ValueError("`delta_bl` must be a scalar or callable")
-
-        if callable(delta_br):
-            self.delta_br = delta_br
-        elif np.isscalar(delta_br):
-            self.delta_br = lambda t: delta_br
-        else:
-            raise ValueError("`delta_br` must be a scalar or callable")
-
-        if callable(delta_w):
-            self.delta_w = delta_w
-        elif np.isscalar(delta_w):
-            self.delta_w = lambda t: delta_w
-        else:
-            raise ValueError("`delta_w` must be a scalar or callable")
-
-        if callable(v_W2e):
-            self.v_W2e = v_W2e
-        elif v_W2e is None:
-            self.v_W2e = lambda t, r: np.zeros_like(r)
-        else:
-            raise ValueError("`v_W2e` must be a callable")
-
-    def cleanup(self, state, t):
-        # FIXME: hack that runs after each integration step. Assumes it can
-        #        modify the integrator state directly.
-        state["q_b2e"] /= np.sqrt((state["q_b2e"] ** 2).sum())  # Normalize
-
-    def dynamics(self, t, y, params):
-        """
-        Compute the state derivatives from the model.
-
-        Matches the `f(t, y, *params)` signature for scipy.integrate.ode
-
-        Parameters
-        ----------
-        t : float [s]
-            Time
-        y : ndarray of float, shape (N,)
-            The array of N state components
-        params : dictionary
-            Any extra non-state parameters for computing the dynamics. Be aware
-            that 'solution' is an in-out parameter: solutions for the current
-            Gamma distribution are passed forward (output) to be used as the
-            proposal to the next time step.
-
-        Returns
-        -------
-        x_dot : ndarray of float, shape (N,)
-            The array of N state component derivatives
-        """
-        x = y.view(self.state_dtype)[0]  # The integrator uses a flat array
-        q_e2b = x["q_b2e"] * [1, -1, -1, -1]  # Encodes `C_ned/frd`
-
-        delta_a = self.delta_a(t)
-        delta_w = self.delta_w(t)
-        r_CP2R = self.glider.control_points(delta_a, delta_w)  # In body frd
-        r_CP2O = x["r_R2O"] + orientation.quaternion_rotate(q_e2b, r_CP2R)
-        v_W2e = self.v_W2e(t, r_CP2O)  # Wind vectors at each ned coordinate
-
-        a_R2e, alpha_b2e, solution = self.glider.accelerations(
-            orientation.quaternion_rotate(x["q_b2e"], x["v_R2e"]),
-            x["omega_b2e"],
-            orientation.quaternion_rotate(x["q_b2e"], [0, 0, 9.8]),
-            rho_air=self.rho_air(t),
-            delta_a=delta_a,
-            delta_bl=self.delta_bl(t),
-            delta_br=self.delta_br(t),
-            delta_w=delta_w,
-            v_W2e=orientation.quaternion_rotate(x["q_b2e"], v_W2e),
-            r_CP2R=r_CP2R,
-            reference_solution=params["solution"],
-        )
-
-        # FIXME: what if Phillips fails? How do I abort gracefully?
-
-        # Quaternion derivative
-        #  * ref: Stevens, Eq:1.8-15, p51 (65)
-        P, Q, R = x["omega_b2e"]
-        # fmt: off
-        Omega = np.array([
-            [0, -P, -Q, -R],
-            [P,  0,  R, -Q],
-            [Q, -R,  0,  P],
-            [R,  Q, -P,  0]])
-        # fmt: on
-        q_dot = 0.5 * Omega @ x["q_b2e"]
-
-        x_dot = np.empty(1, self.state_dtype)
-        x_dot["q_b2e"] = q_dot
-        x_dot["r_R2O"] = x["v_R2e"]
-        x_dot["v_R2e"] = orientation.quaternion_rotate(q_e2b, a_R2e)
-        x_dot["omega_b2e"] = alpha_b2e
-
-        # Use the solution as the reference_solution at the next time step
-        params["solution"] = solution  # FIXME: needs a design review
-
-        return x_dot.view(float)  # The integrator expects a flat array
-
-
-class Dynamics9a:
-    """Defines the state dynamics for a 9 DoF paraglider model."""
-
-    # FIXME: I dislike this notation. It confuses the reference frames with
-    #        the coordinate systems embedded in those frames.
-    state_dtype = [
-        ("q_b2e", float, (4,)),  # Orientation: body/earth
-        ("q_p2e", float, (4,)),  # Orientation: payload/earth
-        ("omega_b2e", float, (3,)),  # Angular velocity of the body in body frd
-        ("omega_p2e", float, (3,)),  # Angular velocity of the payload in payload frd
-        ("r_R2O", float, (3,)),  # The position of `R` in ned
-        ("v_R2e", float, (3,)),  # The velocity of `R` in ned
-    ]
-
-    def __init__(
-        self, glider, rho_air, delta_a=0, delta_bl=0, delta_br=0, delta_w=0, v_W2e=None,
-    ):
-        self.glider = glider
-
-        if callable(rho_air):
-            self.rho_air = rho_air
-        elif np.isscalar(rho_air):
-            self.rho_air = lambda t: rho_air
-        else:
-            raise ValueError("`rho_air` must be a scalar or callable")
-
-        if callable(delta_a):
-            self.delta_a = delta_a
-        elif np.isscalar(delta_a):
-            self.delta_a = lambda t: delta_a
-        else:
-            raise ValueError("`delta_a` must be a scalar or callable")
-
-        if callable(delta_bl):
-            self.delta_bl = delta_bl
-        elif np.isscalar(delta_bl):
-            self.delta_bl = lambda t: delta_bl
-        else:
-            raise ValueError("`delta_bl` must be a scalar or callable")
-
-        if callable(delta_br):
-            self.delta_br = delta_br
-        elif np.isscalar(delta_br):
-            self.delta_br = lambda t: delta_br
-        else:
-            raise ValueError("`delta_br` must be a scalar or callable")
-
-        if callable(delta_w):
-            self.delta_w = delta_w
-        elif np.isscalar(delta_w):
-            self.delta_w = lambda t: delta_w
-        else:
-            raise ValueError("`delta_w` must be a scalar or callable")
-
-        if callable(v_W2e):
-            self.v_W2e = v_W2e
-        elif v_W2e is None:
-            self.v_W2e = lambda t, r: np.zeros_like(r)
-        else:
-            raise ValueError("`v_W2e` must be a callable")
-
-    def cleanup(self, state, t):
-        # FIXME: hack that runs after each integration step. Assumes it can
-        #        modify the integrator state directly.
-        state["q_b2e"] /= np.sqrt((state["q_b2e"] ** 2).sum())  # Normalize
-        state["q_p2e"] /= np.sqrt((state["q_p2e"] ** 2).sum())  # Normalize
-
-    def dynamics(self, t, y, params):
-        """
-        Compute the state derivatives from the model.
-
-        Matches the `f(t, y, *params)` signature for scipy.integrate.ode
-
-        Parameters
-        ----------
-        t : float [s]
-            Time
-        y : ndarray of float, shape (N,)
-            The array of N state components
-        params : dictionary
-            Any extra non-state parameters for computing the dynamics. Be aware
-            that 'solution' is an in-out parameter: solutions for the current
-            Gamma distribution are passed forward (output) to be used as the
-            proposal to the next time step.
-
-        Returns
-        -------
-        x_dot : ndarray of float, shape (N,)
-            The array of N state component derivatives
-        """
-        x = y.view(self.state_dtype)[0]  # The integrator uses a flat array
-        q_e2b = x["q_b2e"] * [-1, 1, 1, 1]  # Encodes `C_ned/frd`
-        Theta_p2b = orientation.quaternion_to_euler(
-            orientation.quaternion_product(q_e2b, x["q_p2e"])
-        )
-
-        delta_a = self.delta_a(t)
-        delta_w = self.delta_w(t)
-        r_CP2R = self.glider.control_points(Theta_p2b, delta_a, delta_w)  # In body frd
-        r_CP2O = x["r_R2O"] + orientation.quaternion_rotate(q_e2b, r_CP2R)
-        v_W2e = self.v_W2e(t, r_CP2O)  # Wind vectors at each ned coordinate
-
-        a_R2e, alpha_b2e, alpha_p2e, solution = self.glider.accelerations(
-            orientation.quaternion_rotate(x["q_b2e"], x["v_R2e"]),
-            x["omega_b2e"],
-            x["omega_p2e"],
-            Theta_p2b,  # FIXME: design review the call signature
-            orientation.quaternion_rotate(x["q_b2e"], [0, 0, 9.8]),
-            rho_air=self.rho_air(t),
-            delta_a=delta_a,
-            delta_bl=self.delta_bl(t),
-            delta_br=self.delta_br(t),
-            delta_w=delta_w,
-            v_W2e=orientation.quaternion_rotate(x["q_b2e"], v_W2e),
-            r_CP2R=r_CP2R,
-            reference_solution=params["solution"],
-        )
-
-        # FIXME: what if Phillips fails? How do I abort gracefully?
-
-        # Quaternion derivatives
-        #  * ref: Stevens, Eq:1.8-15, p51 (65)
-        P, Q, R = x["omega_b2e"]
-        # fmt: off
-        Omega = np.array([
-            [0, -P, -Q, -R],
-            [P,  0,  R, -Q],
-            [Q, -R,  0,  P],
-            [R,  Q, -P,  0]])
-        # fmt: on
-        q_b2e_dot = 0.5 * Omega @ x["q_b2e"]
-
-        P, Q, R = x["omega_p2e"]
-        # fmt: off
-        Omega = np.array([
-            [0, -P, -Q, -R],
-            [P,  0,  R, -Q],
-            [Q, -R,  0,  P],
-            [R,  Q, -P,  0]])
-        # fmt: on
-        q_p2e_dot = 0.5 * Omega @ x["q_p2e"]
-
-        x_dot = np.empty(1, self.state_dtype)
-        x_dot["q_b2e"] = q_b2e_dot
-        x_dot["q_p2e"] = q_p2e_dot
-        x_dot["omega_b2e"] = alpha_b2e
-        x_dot["omega_p2e"] = alpha_p2e
-        x_dot["r_R2O"] = x["v_R2e"]
-        x_dot["v_R2e"] = orientation.quaternion_rotate(q_e2b, a_R2e)
-
-        # Use the solution as the reference_solution at the next time step
-        params["solution"] = solution  # FIXME: needs a design review
-
-        return x_dot.view(float)  # The integrator expects a flat array
-
-
 # ---------------------------------------------------------------------------
 
 
-def simulate(model, state0, T=10, T0=0, dt=0.5, first_step=0.25, max_step=0.5):
+def linear_control(pairs):
     """
-    Generate a state trajectory using the model dynamics.
+    Helper funtion to build linear interpolators for control inputs.
+
+    The input is a sequence of tuples encoding  `(duration, value)`. An initial
+    value can be set with a leading `(0, initial_value)` tuple. To "hold" a
+    value, use `None` to repeat the previous value.
+
+    For example, to ramp from 0 to 0.5 over the initial 15 seconds, then
+    transition to 0.75 over a period of 2 seconds, hold for 10 seconds, then
+    decrease to 0 over 10 seconds:
+
+        pairs = [(15, 0.5), (2, 0.75), (10, None), (10, 0)]
 
     Parameters
     ----------
-    model
-        The model that provides `dynamics` and `state_dtype`
-    state0 : model.state_dtype
-        The initial state
-    T : float [seconds]
-        The total simulation time
-    T0 : float [seconds]
-        The start time of the simulation. Useful for models with time varying
-        behavior (eg, wind fields).
-    dt : float [seconds]
-        The simulation step size. This determines the time separation of each
-        point in the state trajectory, but the RK4 integrator is free to use
-        a different step size internally.
-
-    Returns
-    -------
-    times : array of float, shape (K+1,) [seconds]
-        The timestamp of each solution
-    path : array of `model.state_dtype`, shape (K+1,)
-        The state trajectory.
+    pairs : list of 2-tuples of float
+        Each tuple is (duration, value).
     """
-    num_steps = int(np.ceil(T / dt)) + 1  # Include the initial state
-    times = np.zeros(num_steps)  # The simulation times
-    path = np.empty(num_steps, dtype=model.state_dtype)
-    path[0] = state0
-
-    solver = scipy.integrate.ode(model.dynamics)
-    solver.set_integrator("dopri5", rtol=1e-5, first_step=0.25, max_step=0.5)
-    solver.set_initial_value(state0.view(float))
-    solver.set_f_params({"solution": None})  # Is modified by `model.dynamics`
-
-    t_start = time.perf_counter()
-    msg = ""
-    k = 1  # Number of completed states (including the initial state)
-    print("\nRunning the simulation.")
-    try:
-        while solver.successful() and k < num_steps:
-            if k % 25 == 0:  # Update every 25 iterations
-                avg_rate = (k - 1) / (time.perf_counter() - t_start)  # k=0 was free
-                rem = (num_steps - k) / avg_rate  # Time remaining in seconds
-                msg = f"ETA: {int(rem // 60)}m{int(rem % 60):02d}s"
-            print(f"\rStep: {k} (t = {k*dt:.2f}). {msg}", end="")
-
-            # WARNING: `solver.integrate` returns a *reference* to `_y`, so
-            #          modifying `state` modifies `solver._y` directly.
-            # FIXME: Is that valid for all `integrate` methods (eg, Adams)?
-            #        Using `solver.set_initial_value` would reset the
-            #        integrator, but that's not what we want here.
-            state = solver.integrate(solver.t + dt).view(model.state_dtype)
-            model.cleanup(state, solver.t)  # Modifies `solver._y`!!
-            path[k] = state  # Makes a copy of `solver._y`
-            times[k] = solver.t
-            k += 1
-    except RuntimeError as e:  # The model blew up
-        # FIXME: refine this idea
-        print(f"\n--- Simulation failed: {type(e).__name__}:", e)
-    except KeyboardInterrupt:
-        print("\n--- Simulation interrupted. ---")
-    finally:
-        if k < num_steps:  # Truncate if the simulation did not complete
-            times = times[:k]
-            path = path[:k]
-
-    print(f"\nTotal simulation time: {time.perf_counter() - t_start}\n")
-
-    return times, path
+    durations = np.array([t[0] for t in pairs])
+    values = [t[1] for t in pairs]
+    assert all(durations >= 0)
+    for n, v in enumerate(values):  # Use `None` for "hold previous value"
+        values[n] = v if v is not None else values[n - 1]
+    times = np.cumsum(durations)
+    c = interp1d(times, values, fill_value=(values[0], values[-1]), bounds_error=False)
+    return c
 
 
 def main():
-    # -----------------------------------------------------------------------
     # Build the glider
-
     wing = hook3.build_hook3()
     harness = gsim.harness.Spherical(
         mass=75, z_riser=0.5, S=0.55, CD=0.8, kappa_w=0.1,
@@ -532,7 +168,7 @@ def main():
     # )
 
     q_b2e_6a = orientation.euler_to_quaternion(equilibrium_6a["Theta_b2e"])
-    state_6a = np.empty(1, dtype=Dynamics6a.state_dtype)
+    state_6a = np.empty(1, dtype=simulator.Dynamics6a.state_dtype)
     state_6a["q_b2e"] = q_b2e_6a
     state_6a["omega_b2e"] = [np.deg2rad(0), np.deg2rad(0), np.deg2rad(0)]
     state_6a["r_R2O"] = [0, 0, 0]
@@ -541,7 +177,7 @@ def main():
     )
 
     q_b2e_9a = orientation.euler_to_quaternion(equilibrium_9a["Theta_b2e"])
-    state_9a = np.empty(1, dtype=Dynamics9a.state_dtype)
+    state_9a = np.empty(1, dtype=simulator.Dynamics9a.state_dtype)
     state_9a["q_b2e"] = q_b2e_9a
     q_p2b_9a = orientation.euler_to_quaternion(equilibrium_9a["Theta_p2b"])
     state_9a["q_p2e"] = orientation.quaternion_product(q_b2e_9a, q_p2b_9a)
@@ -646,12 +282,12 @@ def main():
     # Build the dynamics models
 
     common_args = (rho_air, delta_a, delta_bl, delta_br, delta_w, v_W2e)
-    model_6a = Dynamics6a(glider_6a, *common_args)
-    model_6b = Dynamics6a(glider_6b, *common_args)
-    model_6c = Dynamics6a(glider_6c, *common_args)
-    model_9a = Dynamics9a(glider_9a, *common_args)
-    model_9b = Dynamics9a(glider_9b, *common_args)
-    model_9c = Dynamics9a(glider_9c, *common_args)
+    model_6a = simulator.Dynamics6a(glider_6a, *common_args)
+    model_6b = simulator.Dynamics6a(glider_6b, *common_args)
+    model_6c = simulator.Dynamics6a(glider_6c, *common_args)
+    model_9a = simulator.Dynamics9a(glider_9a, *common_args)
+    model_9b = simulator.Dynamics9a(glider_9b, *common_args)
+    model_9c = simulator.Dynamics9a(glider_9c, *common_args)
 
     # Choose which model to run
     state0, model = state_6a, model_6a
@@ -683,7 +319,7 @@ def main():
 
     t_start = time.perf_counter()
     dt = 0.10  # Time step for the `path` trajectory
-    times, path = simulate(model, state0, dt=dt, T=T)
+    times, path = simulator.simulate(model, state0, dt=dt, T=T)
 
     # -----------------------------------------------------------------------
     # Extra values for verification/debugging
