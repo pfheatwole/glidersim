8f229965494b90a3e2c085d4085b4c86814ca4e6
Peter Heatwole <pfheatwole@gmail.com>
2021-01-04 21:43:53 -0800

    Strip `compute_polars.py` to just the plots

--- src/pfh/glidersim/extras/compute_polars.py
+++ src/pfh/glidersim/extras/compute_polars.py
@@ -1,10 +1,7 @@
-from pathlib import Path
-
 import matplotlib.pyplot as plt
 import numpy as np
 
-import pfh.glidersim as gsim
-from pfh.glidersim import orientation
+from pfh.glidersim import foil, orientation
 
 
 def plot_polar_curve(glider, N=21, approximate=True):
@@ -36,7 +33,7 @@ def plot_polar_curve(glider, N=21, approximate=True):
                 rho_air=1.2,
                 reference_solution=equilibrium["reference_solution"],
             )
-        except gsim.foil.ForceEstimator.ConvergenceError:
+        except foil.ForceEstimator.ConvergenceError:
             print("\nConvergence started failing. Aborting early.")
             delta_as = delta_as[:n]
             break
@@ -64,7 +61,7 @@ def plot_polar_curve(glider, N=21, approximate=True):
                 rho_air=1.2,
                 reference_solution=equilibrium["reference_solution"],
             )
-        except gsim.foil.ForceEstimator.ConvergenceError:
+        except foil.ForceEstimator.ConvergenceError:
             print("\nConvergence started failing. Aborting early.")
             delta_bs = delta_bs[:n]
             break
@@ -143,7 +140,7 @@ def plot_wing_coefficients(wing, delta_b=0, v_mag=10, rho_air=1.2):
             )
             Fs.append(dF.sum(axis=0))
             Ms.append(dM.sum(axis=0))
-        except gsim.foil.ForceEstimator.ConvergenceError:
+        except foil.ForceEstimator.ConvergenceError:
             break
     alphas = alphas[:k]
     Fs = Fs[:k]
@@ -191,218 +188,3 @@ def plot_wing_coefficients(wing, delta_b=0, v_mag=10, rho_air=1.2):
     fig.tight_layout()  # Prevent axes overlapping titles
     plt.show()
     breakpoint()
-
-
-def build_hook3():
-
-    print("Building an (approximate) Niviuk Hook 3 23\n")
-
-    # -----------------------------------------------------------------------
-    # Airfoil
-
-    print("Airfoil: NACA 24018, curving flap\n")
-    airfoil = gsim.extras.airfoils.load_airfoil()  # WIP: placeholder
-    delta_max = np.deg2rad(13.37)  # FIXME: magic number
-
-    # -----------------------------------------------------------------------
-    # Parafoil: an approximate Niviuk Hook 3, size 23
-
-    # True technical specs
-    chord_tip, chord_root, chord_mean = 0.52, 2.58, 2.06
-    S_flat, b_flat, AR_flat = 23, 11.15, 5.40
-    SMC_flat = b_flat / AR_flat
-    S, b, AR = 19.55, 8.84, 4.00
-
-    c = gsim.foil.elliptical_chord(
-        root=chord_root / (b_flat / 2),
-        tip=chord_tip / (b_flat / 2),
-    )
-
-    # The geometric torsion distribution is uncertain. In Sec. 11.4, pg 17 of
-    # the manual ("Line Plan") it appears to have a roughly square-root
-    # spanwise distribution with a maximum valueof 6 or so? Unfortunately that
-    # distribution proves difficult for Phillips method, so here I provide an
-    # "easier" alternative.
-    # theta = Parafoil.PolynomialTorsion(start=0.0, peak=6, exponent=0.75)
-    theta = gsim.foil.PolynomialTorsion(start=0.8, peak=4, exponent=2)
-
-    layout = gsim.foil.SectionLayout(
-        r_x=0.75,
-        x=0,
-        r_yz=1.00,
-        yz=gsim.foil.elliptical_arc(mean_anhedral=33, tip_anhedral=67),
-        c=c,
-        theta=theta,
-    )
-
-    # FIXME: add the viscous drag modifiers?
-    sections = gsim.foil.FoilSections(
-        airfoil=airfoil,
-        intakes=gsim.foil.SimpleIntakes(0.85, -0.04, -0.09),  # FIXME: guess
-    )
-
-    canopy = gsim.foil.SimpleFoil(
-        layout=layout,
-        sections=sections,
-        # b=b,  # Option 1: Scale the using the projected span
-        b_flat=b_flat,  # Option 2: Scale the using the flattened span
-    )
-
-    print("Canopy geometry:           [Target]")
-    print(f"  flattened span: {canopy.b_flat:>6.3f}   [{b_flat:>6.3f}]")
-    print(f"  flattened area: {canopy.S_flat:>6.3f}   [{S_flat:>6.3f}]")
-    print(f"  flattened AR:   {canopy.AR_flat:>6.3f}   [{AR_flat:>6.3f}]")
-    # print(f"  planform flat SMC   {canopy.SMC:>6.3f}")
-    # print(f"  planform flat MAC:  {canopy.MAC:>6.3f}")
-
-    print(f"  projected span: {canopy.b:>6.3f}   [{b:>6.3f}]")
-    print(f"  projected area: {canopy.S:>6.3f}   [{S:>6.3f}]")
-    print(f"  projected AR:   {canopy.AR:>6.3f}   [{AR:>6.3f}]")
-    print()
-
-    # print("Drawing the canopy")
-    # gsim.plots.plot_foil(canopy, N_sections=131, surface="airfoil", flatten=False)
-    # gsim.plots.plot_foil(canopy, N_sections=71, surface="airfoil", flatten=True)
-    # gsim.plots.plot_foil(canopy, N_sections=131, surface="chord", flatten=False)
-    # gsim.plots.plot_foil(canopy, N_sections=71, surface="chord", flatten=True)
-    # gsim.plots.plot_foil(canopy, N_sections=131, surface="camber", flatten=False)
-    # gsim.plots.plot_foil(canopy, N_sections=71, surface="camber", flatten=True)
-    # gsim.plots.plot_foil_topdown(canopy, N_sections=51)
-    # gsim.plots.plot_foil_topdown(canopy, N_sections=51, flatten=True)
-
-    # print("\nPausing...")
-    # breakpoint()
-
-    # Compare to the Hook 3 manual, sec 11.4 "Line Plan", page 17
-    # gsim.plots.plot_foil_topdown(canopy, N_sections=77)
-
-    # -----------------------------------------------------------------------
-    # Line geometry
-    #
-    # The brake parameters are not based on the actual wing in any way.
-    # The line drag positions are crude guess.
-
-    line_parameters = {
-        "kappa_x": 0.49,  # FIXME: Source? Trying to match `theta_eq` at trim?
-        "kappa_z": 6.8 / chord_root,  # ref: "Hook 3 technical specs", pg 2
-        "kappa_A": 0.11,  # Approximated from the line plan in the users manual, pg 17
-        "kappa_C": 0.59,
-        "kappa_a": 0.15 / chord_root,  # ref: "Hook 3 technical specs", pg 2
-        "total_line_length": 213 / chord_root,  # ref: "Hook 3 technical specs", pg 2
-        "average_line_diameter": 1e-3,  # Blind guess
-        "r_L2LE": np.array([[-0.5 * chord_root, -1.75, 1.75],
-                            [-0.5 * chord_root,  1.75, 1.75]]) / chord_root,
-        "Cd_lines": 0.98,  # ref: Kulhánek, 2019; page 5
-    }
-
-    s_delta_start = 0.1
-    s_delta_max = gsim.line_geometry.SimpleLineGeometry.minimum_s_delta_max(s_delta_start) + 1e-9
-    brake_parameters = {
-        "s_delta_start": s_delta_start,
-        "s_delta_max": s_delta_max,
-        "delta_max": delta_max,
-    }
-
-    lines = gsim.line_geometry.SimpleLineGeometry(
-        **line_parameters,
-        **brake_parameters,
-    )
-
-    # -----------------------------------------------------------------------
-    # Wing and glider
-
-    wing = gsim.paraglider_wing.ParagliderWing(
-        lines=lines,
-        canopy=canopy,
-        N_cells=52,
-        rho_upper=39 / 1000,  # [kg/m^2]  Porcher 9017 E77A
-        rho_lower=35 / 1000,  # [kg/m^2]  Dominico N20DMF
-        rho_ribs=41 / 1000,   # [kg/m^2]  Porcher 9017 E29
-        force_estimator=gsim.foil.Phillips(canopy, v_ref_mag=10, K=31),
-    )
-
-    return wing
-
-
-if __name__ == "__main__":
-
-    print("\n-------------------------------------------------------------\n")
-
-    # Note to self: the wing should weight 4.7kG in total; according to these
-    # specs, and the `rho_upper`/`rho_lower` embedded in ParagliderWing, the
-    # wing materials I'm accounting for total to 1.83kg, so there's a lot left
-    # in the lines, risers, ribs, etc.
-    wing = build_hook3()
-    harness = gsim.harness.Spherical(
-        mass=75, z_riser=0.5, S=0.55, CD=0.8, kappa_w=0.15,
-    )
-    glider = gsim.paraglider.Paraglider6a(wing, harness)
-    # glider = gsim.paraglider.Paraglider9a(wing, harness)
-
-    # print("Plotting the wing performance curves")
-    # plot_wing_coefficients(wing)
-
-    print("\nFinished building the glider.\n")
-    # breakpoint()
-    # 1/0
-
-    print("\nComputing the glider equilibrium state...")
-
-    approximate = True
-    # approximate = False
-
-    if approximate:  # Option 1: fast, but only approximate
-        eqstate = glider.equilibrium_state2
-    else:  # Option 2: currently very slow, but more accurate
-        eqstate = glider.equilibrium_state
-
-    eq = eqstate(
-        delta_a=0.0,
-        delta_b=0.0,
-        alpha_0=np.deg2rad(9),
-        theta_0=np.deg2rad(3),
-        v_0=10,
-        rho_air=1.2,
-    )
-
-    # Compute the residual acceleration at the given equilibrium state
-    q_b2e = orientation.euler_to_quaternion(eq["Theta_b2e"])
-    q_e2b = q_b2e * [-1, 1, 1, 1]
-    v_RM2e = orientation.quaternion_rotate(q_e2b, eq["v_RM2e"])
-
-    # For the `Paraglider6a` model
-    a_RM2e, alpha_b2e, _ = glider.accelerations(
-        v_RM2e=eq["v_RM2e"],
-        omega_b2e=[0, 0, 0],
-        g=orientation.quaternion_rotate(q_b2e, [0, 0, 9.8]),
-        rho_air=1.2,
-        reference_solution=eq["reference_solution"],
-    )
-    # For the `Paraglider9a` model
-    # a_RM2e, alpha_b2e, alpha_p2b, _ = glider.accelerations(
-    #     v_RM2e=eq["v_RM2e"],
-    #     omega_b2e=[0, 0, 0],
-    #     omega_p2e=[0, 0, 0],
-    #     Theta_p2b=eq["Theta_p2b"],
-    #     g=orientation.quaternion_rotate(q_b2e, [0, 0, 9.8]),
-    #     rho_air=1.2,
-    #     reference_solution=eq["reference_solution"],
-    # )
-
-    print("Equilibrium state:")
-    print(f"  alpha_b:     {np.rad2deg(eq['alpha_b']):>6.3f} [deg]")
-    print(f"  theta_b:     {np.rad2deg(eq['Theta_b2e'][1]):>6.3f} [deg]")
-    print(f"  Glide angle: {np.rad2deg(eq['gamma_b']):>6.3f} [deg]")
-    print(f"  Glide ratio: {eq['glide_ratio']:>6.3f}")
-    print(f"  Glide speed: {np.linalg.norm(v_RM2e):>6.3f}")
-    print()
-    print("For verification of the equilibrium state:")
-    print(f"  v_RM2e:      {v_RM2e.round(4)}")
-    print(f"  a_RM2e:      {a_RM2e.round(4)}")
-    print(f"  alpha_b2e:   {np.rad2deg(alpha_b2e).round(4)}")
-
-    print("\n<pausing before polar curves>\n")
-    breakpoint()
-
-    input("Plot the polar curve?  Press any key")
-    plot_polar_curve(glider, approximate=approximate)
