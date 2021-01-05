79aa541da8d724180acd13c08d22b4a79388f8a5
Peter Heatwole <pfheatwole@gmail.com>
2021-01-05 09:58:15 -0800

    Tidy `load_datfile` extras

--- src/pfh/glidersim/extras/airfoils.py
+++ src/pfh/glidersim/extras/airfoils.py
@@ -6,7 +6,8 @@ import pfh.glidersim as gsim
 
 __all__ = [
     "load_airfoil",
-    "load_airfoil_set",
+    "load_datfile",
+    "load_datfile_set",
 ]
 
 
@@ -23,13 +24,57 @@ def load_airfoil():
     return airfoil
 
 
-def load_airfoil_set(directory):
+def load_datfile(
+    path, convention="perpendicular", center=False, derotate=False, normalize=False
+):
     """
-    Load a set of airfoil `.dat` files using numpy. (Crude! Beware!)
+    Build an AirfoilGeometry from a `.dat` file using numpy.
+
+    Parameters
+    ----------
+    path : string
+        The filename.
+    convention : {"perpendicular", "vertical"}, optional
+        Whether the airfoil thickness is measured perpendicular to the mean
+        camber line or vertically (the y-axis distance). Default: perpendicular
+    center : bool, optional
+        Translate the curve leading edge to the origin. Default: True
+    derotate : bool, optional
+        Rotate the the chord parallel to the x-axis. Default: True
+    normalize : bool, optional
+        Scale the curve so the chord is unit length. Default: True
+
+    Returns
+    -------
+    airfoil : gsim.airfoil.AirfoilGeometry
+        The instantiated AirfoilGeometry object.
+    """
+    points = np.loadtxt(path, skiprows=1)
+    airfoil = gsim.airfoil.AirfoilGeometry.from_points(
+        points,
+        convention=convention,
+        center=center,
+        derotate=derotate,
+        normalize=normalize,
+    )
+    return airfoil
+
+
+def load_datfile_set(
+    directory, convention="perpendicular", center=False, derotate=False, normalize=False
+):
+    """
+    Load a set of airfoil `.dat` files using numpy.
 
     This is intended for use with a set of .dat files that represent a single
     airfoil geometry with deflected trailing edges. The deflection angle must
-    be the last part of the filename
+    be the last part of the filename.
+
+    For example, the directory might contain:
+
+      naca23015_delta0.00.dat
+      naca23015_delta5.00.dat
+      naca23015_delta10.0.dat
 
     Parameters
     ----------
@@ -37,39 +82,36 @@ def load_airfoil_set(directory):
         The directory containing a set of airfoil curves in the typical `.dat`
         format. The filenames must end with `_delta<###>`, where `<###>` is a
         floating point number
+    convention : {"perpendicular", "vertical"}, optional
+        Whether the airfoil thickness is measured perpendicular to the mean
+        camber line or vertically (the y-axis distance). Default: perpendicular
+    center : bool, optional
+        Translate the curve leading edge to the origin. Default: True
+    derotate : bool, optional
+        Rotate the the chord parallel to the x-axis. Default: True
+    normalize : bool, optional
+        Scale the curve so the chord is unit length. Default: True
 
     Returns
     -------
     airfoils : dict
-        A dictionary, keyed by the value of `delta`. Each value of `delta` is
-        a dictionary for each datfile containing the values:
-
-        name: string
-            The airfoil name (minus the `_delta<###>.dat`)
-        points: array of float, shape (K,2)
-            The xy-coordinates of the airfoil curve.
-        airfoil : gsim.airfoil.AirfoilGeometry
-            The instantiated AirfoilGeometry object.
+        A dictionary of {delta: gsim.airfoil.AirfoilGeometry} pairs.
     """
     datdir = Path(directory)
     airfoils = {}
-    for datfile in datdir.glob("*.dat"):
+    for datfile in sorted(datdir.glob("*.dat")):
         # Filenames are expected to follow `<basename>_delta<#######>.dat`.
         # The number of digits in `delta` is variable.
         datname = datfile.name
         delta_start = datname.find("_delta")
-        delta = float(datname[delta_start+6:-4])
-        points = np.loadtxt(datfile, skiprows=1)
-        airfoils[delta] = {"name": datname[:delta_start], "points": points}
-
-    for key in sorted(airfoils.keys()):
-        airfoil = gsim.airfoil.AirfoilGeometry.from_points(
-            airfoils[key]["points"],
-            convention="perpendicular",
-            center=False,
-            derotate=False,
-            normalize=False,
+        delta = float(datname[delta_start + 6 : -4])
+        airfoil = load_datfile(
+            datfile,
+            convention=convention,
+            center=center,
+            derotate=derotate,
+            normalize=normalize,
         )
-        airfoils[key]["airfoil"] = airfoil
+        airfoils[delta] = {"name": datname, "airfoil": airfoil}
 
     return airfoils
