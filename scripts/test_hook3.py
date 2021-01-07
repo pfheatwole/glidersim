1d45980ce15e211c65b2d66c2c34dcb37c8aac6d
Peter Heatwole <pfheatwole@gmail.com>
2021-01-06 17:10:52 -0800

    Tidy test_hook3 handling of `6a` vs `9a`
    Still ugly, still functional.

--- scripts/test_hook3.py
+++ scripts/test_hook3.py
@@ -9,15 +9,19 @@ if __name__ == "__main__":
 
     print("\n-------------------------------------------------------------\n")
 
-    wing = gsim.extras.wings.build_hook3()
+    use_6a = True
+    # use_6a = False  # Disable to use Paraglider9a
+
+    wing = gsim.extras.wings.build_hook3(verbose=True)
     harness = gsim.harness.Spherical(
         mass=75, z_riser=0.5, S=0.55, CD=0.8, kappa_w=0.15,
     )
-    glider = gsim.paraglider.Paraglider6a(wing, harness)
-    # glider = gsim.paraglider.Paraglider9a(wing, harness)
+    if use_6a:
+        glider = gsim.paraglider.Paraglider6a(wing, harness)
+    else:
+        glider = gsim.paraglider.Paraglider9a(wing, harness)
 
-    print("Finished building the glider.\n")
-    print("\nComputing the glider equilibrium state...\n")
+    print("Computing the glider equilibrium state...\n")
     eq = glider.equilibrium_state()
 
     # Compute the residual acceleration at the given equilibrium state
@@ -25,26 +29,29 @@ if __name__ == "__main__":
     q_e2b = q_b2e * [-1, 1, 1, 1]
     v_RM2e = orientation.quaternion_rotate(q_e2b, eq["v_RM2e"])
 
-    # For the `Paraglider6a` model
-    a_RM2e, alpha_b2e, _ = glider.accelerations(
-        v_RM2e=eq["v_RM2e"],
-        omega_b2e=[0, 0, 0],
-        g=orientation.quaternion_rotate(q_b2e, [0, 0, 9.8]),
-        reference_solution=eq["reference_solution"],
-    )
-    # For the `Paraglider9a` model
-    # a_RM2e, alpha_b2e, alpha_p2b, _ = glider.accelerations(
-    #     v_RM2e=eq["v_RM2e"],
-    #     omega_b2e=[0, 0, 0],
-    #     omega_p2e=[0, 0, 0],
-    #     Theta_p2b=eq["Theta_p2b"],
-    #     g=orientation.quaternion_rotate(q_b2e, [0, 0, 9.8]),
-    #     reference_solution=eq["reference_solution"],
-    # )
+    if use_6a:
+        a_RM2e, alpha_b2e, _ = glider.accelerations(
+            v_RM2e=eq["v_RM2e"],
+            omega_b2e=[0, 0, 0],
+            g=orientation.quaternion_rotate(q_b2e, [0, 0, 9.8]),
+            reference_solution=eq["reference_solution"],
+        )
+    else:
+        a_RM2e, alpha_b2e, alpha_p2b, _ = glider.accelerations(
+            v_RM2e=eq["v_RM2e"],
+            omega_b2e=[0, 0, 0],
+            omega_p2e=[0, 0, 0],
+            Theta_p2b=eq["Theta_p2b"],
+            g=orientation.quaternion_rotate(q_b2e, [0, 0, 9.8]),
+            reference_solution=eq["reference_solution"],
+        )
 
     print("Equilibrium state:")
     print(f"  alpha_b:     {np.rad2deg(eq['alpha_b']):>6.3f} [deg]")
-    print(f"  theta_b:     {np.rad2deg(eq['Theta_b2e'][1]):>6.3f} [deg]")
+    print(f"  theta_b2e:   {np.rad2deg(eq['Theta_b2e'][1]):>6.3f} [deg]")
+    if use_6a is False:
+        theta_p2e = eq["Theta_p2b"][1] + eq["Theta_b2e"][1]
+        print(f"  theta_p2e:   {np.rad2deg(theta_p2e):>6.3f} [deg]")
     print(f"  Glide angle: {np.rad2deg(eq['gamma_b']):>6.3f} [deg]")
     print(f"  Glide ratio: {eq['glide_ratio']:>6.3f}")
     print(f"  Glide speed: {np.linalg.norm(v_RM2e):>6.3f}")
@@ -52,6 +59,8 @@ if __name__ == "__main__":
     print("Verify accelerations at equilibrium:")
     print(f"  a_RM2e:      {a_RM2e.round(4)}")
     print(f"  alpha_b2e:   {np.rad2deg(alpha_b2e).round(4)}")
+    if use_6a is False:
+        print(f"  alpha_p2b:   {np.rad2deg(alpha_p2b).round(4)}")
 
     print("\n<pausing before polar curves>\n")
     breakpoint()
